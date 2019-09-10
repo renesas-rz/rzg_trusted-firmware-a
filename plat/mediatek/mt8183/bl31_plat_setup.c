jolivain/trusted-firmware-a

mediatek: mt8183: pass platform parameters
Add plat parameter structs to support BL2 to pass
variable-length, variable-type parameters to BL31.
The parameters are structured as a link list.
During BL31 setup time, we traverse the list to process
each parameter.

Signed-off-by: kenny liang <kenny.liang@mediatek.com>
Change-Id: Ie84cfc9606656fb1d2780a68cadf27e09afa6628

plat/mediatek/mt8183/bl31_plat_setup.c
@@ -15,6 +15,7 @@
 #include <lib/mmio.h>
 #include <mtk_plat_common.h>
 #include <mtspmc.h>
+#include <params_setup.h>
 #include <plat_debug.h>
 #include <plat_private.h>
 #include <platform_def.h>
@@ -68,6 +69,9 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 								u_register_t arg2, u_register_t arg3)
 {
 	struct mtk_bl31_params *arg_from_bl2 = (struct mtk_bl31_params *)arg0;
+	void *plat_params_from_bl2 = (void *) arg1;
+
+	params_early_setup(plat_params_from_bl2);
 
 	static console_16550_t console;
 	console_16550_register(UART0_BASE, UART_CLOCK, UART_BAUDRATE, &console);

renesas-rz/rzg_trusted-firmware-a

plat/mediatek/mt8183/bl31_plat_setup.c
@@ -18,6 +18,7 @@
 #include <mtk_plat_common.h>
 #include <mtspmc.h>
 #include <plat_debug.h>
+#include <plat_params.h>
 #include <plat_private.h>
 #include <platform_def.h>
 #include <scu.h>
@@ -73,6 +74,8 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 				u_register_t arg2, u_register_t arg3)
 {
 	static console_16550_t console;
+
+	params_early_setup(arg1);
 
 	console_16550_register(UART0_BASE, UART_CLOCK, UART_BAUDRATE, &console);
 