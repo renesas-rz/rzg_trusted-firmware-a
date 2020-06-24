jolivain/trusted-firmware-a

fconf: Allow fconf to load additional firmware configuration
Modified the `fconf_load_config` function so that it can
additionally support loading of tb_fw_config along with
fw_config.

Signed-off-by: Louis Mayencourt <louis.mayencourt@arm.com>
Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
Change-Id: Ie060121d367ba12e3fcac5b8ff169d415a5c2bcd

include/lib/fconf/fconf.h
@@ -43,8 +43,8 @@ struct fconf_populator {
 	int (*populate)(uintptr_t config);
 };
 
-/* Load firmware configuration dtb */
-void fconf_load_config(void);
+/* This function support to load tb_fw_config and fw_config dtb */
+void fconf_load_config(unsigned int config_id);
 
 /* Top level populate function
  *

renesas-rz/rzg_trusted-firmware-a

include/lib/fconf/fconf.h
@@ -43,8 +43,8 @@ struct fconf_populator {
 	int (*populate)(uintptr_t config);
 };
 
-/* Load firmware configuration dtb */
-void fconf_load_config(void);
+/* This function supports to load tb_fw_config and fw_config dtb */
+void fconf_load_config(unsigned int image_id);
 
 /* Top level populate function
  *