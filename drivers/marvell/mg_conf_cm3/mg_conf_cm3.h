Gateworks/atf-newport

drivers: marvell: mg_conf_cm3: pass comphy lane number to AP FW
Since the AP process can be enabled on different setups, the information
about used comphy lane should be passed to AP FW. For instance:
- A8K development board uses comphy lane 2 for eth 0
- cn913x development board uses comphy lane 4 for eth 0

Change-Id: I8d5f11a360f08981e1a5f83007501c009a38c756
Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
Reviewed-on: https://sj1git1.cavium.com/11333
Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

drivers/marvell/mg_conf_cm3/mg_conf_cm3.h
@@ -5,5 +5,5 @@
  * https://spdx.org/licenses
  */
 
-void mg_start_ap_fw(int cp_nr);
+void mg_start_ap_fw(int cp_nr, uint8_t cp_index);
 int mg_image_load(uintptr_t src_addr, uint32_t size, int cp_index);

renesas-rz/rzg_trusted-firmware-a

Change-Id: Icf001fb3eea4d9c24c09384e49844ecaf8655ad2

drivers/marvell/mg_conf_cm3/mg_conf_cm3.h
@@ -5,5 +5,5 @@
  * https://spdx.org/licenses
  */
 
-void mg_start_ap_fw(int cp_nr);
+void mg_start_ap_fw(int cp_nr, uint8_t comphy_index);
 int mg_image_load(uintptr_t src_addr, uint32_t size, int cp_index);