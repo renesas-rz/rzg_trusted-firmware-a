f2fd7089ac160812eee98dd154548b7d041bc214
Takeshi Naruse <takeshi.naruse.te@bp.renesas.com>
2020-10-19 13:41:41 +0900

    1st commit

---
+++ plat/renesas/rzg2l/bl2_plat_mem_params_desc.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/desc_image_load.h>
+
+static bl_mem_params_node_t bl2_mem_params_descs[] = {
+};
+
+REGISTER_BL_IMAGE_DESCS(bl2_mem_params_descs)
---
+++ plat/renesas/rzg2l/drivers/pfc.c
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
