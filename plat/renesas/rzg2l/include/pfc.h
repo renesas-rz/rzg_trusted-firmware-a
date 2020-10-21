c1a9b6ba2d1ae522f1b377bd24bee504a9ef4aaa
hiromasa kakutani <hiromasa.kakutani.pv@bp.renesas.com>
2020-10-21 16:07:17 +0900

    ddition of cpg and pfc

---
+++ plat/renesas/rzg2l/include/pfc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifdef _PFC_H_
+#define _PFC_H_
+
+extern void pfc_setup(void);
+
+
+#endif	/* _PFC_H_ */
