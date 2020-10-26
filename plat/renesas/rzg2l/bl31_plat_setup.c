28b8b77e5f3456c3e3c5d6ae7e0e44ec55154c2b
Takeshi Naruse <takeshi.naruse.te@bp.renesas.com>
2020-10-26 15:47:49 +0900

    Add coldboot function of BL31

--- plat/renesas/rzg2l/bl31_plat_setup.c
+++ plat/renesas/rzg2l/bl31_plat_setup.c
@@ -11,16 +11,39 @@
 #include <lib/xlat_tables/xlat_tables_compat.h>
 
 #include "syc.h"
+#include "scifa.h"
+#include "rzg2l_private.h"
 
 static const mmap_region_t rzg2l_mmap[] = {
+	MAP_REGION_FLAT(RZG2L_DEVICE_BASE, RZG2L_DEVICE_SIZE,
+			MT_DEVICE | MT_RW | MT_SECURE),
+	MAP_REGION_FLAT(RZG2L_SPIROM_BASE, RZG2L_SPIROM_SIZE,
+			MT_MEMORY | MT_RO | MT_SECURE),
+	MAP_REGION_FLAT(RZG2L_DDR1_BASE, RZG2L_DDR1_SIZE,
+			MT_MEMORY | MT_RW | MT_SECURE),
 	{0}
 };
+
+static console_t rzg2l_bl31_console;
 
 void bl31_early_platform_setup2(u_register_t arg0,
 								u_register_t arg1,
 								u_register_t arg2,
 								u_register_t arg3)
 {
+	int ret;
+
+	/* initialize console driver */
+	ret = console_rzg2l_register(
+							RZG2L_SCIF0_BASE,
+							RZG2L_UART_INCK_HZ,
+							RZG2L_UART_BARDRATE,
+							&rzg2l_bl31_console);
+	if(!ret)
+		panic();
+
+	console_set_scope(&rzg2l_bl31_console,
+			CONSOLE_FLAG_BOOT | CONSOLE_FLAG_RUNTIME | CONSOLE_FLAG_CRASH);
 }
 
 void bl31_plat_arch_setup(void)
@@ -30,11 +53,6 @@ void bl31_plat_arch_setup(void)
 						MT_MEMORY | MT_RW | MT_SECURE),
 		MAP_REGION_FLAT(BL_CODE_BASE, BL_CODE_END - BL_CODE_BASE,
 						MT_CODE | MT_SECURE),
-#if USE_COHERENT_MEM
-		MAP_REGION_FLAT(BL_COHERENT_RAM_BASE,
-						BL_COHERENT_RAM_END - BL_COHERENT_RAM_BASE,
-						MT_DEVICE | MT_RW | MT_SECURE),
-#endif
 		{0}
 	};
 
@@ -44,10 +62,17 @@ void bl31_plat_arch_setup(void)
 
 void bl31_platform_setup(void)
 {
+	/* initialize GIC-600 */
+	plat_gic_driver_init();
+	plat_gic_init();
+
+	/* initialize SYC driver */
+	syc_init(RZG2L_SYSC_BASE, RZG2L_SYC_INCK_HZ);
 }
 
 entry_point_info_t *bl31_plat_get_next_image_ep_info(uint32_t type)
 {
+	/* TODO , Implement */
 	entry_point_info_t *ep = NULL;
 
 	return ep;
---
+++ plat/renesas/rzg2l/include/rzg2l_private.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __RZG2L_PRIVATE_H__
+#define __RZG2L_PRIVATE_H__
+
+#define RZG2L_DEVICE_BASE			(0x10000000)
+#define RZG2L_SCIF0_BASE			(0x1004B800)
+#define RZG2L_SPIMULT_BASE			(0x10060000)
+#define RZG2L_SPIMULT_WBUF_BASE		(0x10070000)
+#define RZG2L_SYC_BASE				(0x11020000)
+#define RZG2L_CPG_BASE				(0x11010000)
+#define RZG2L_SYSC_BASE				(0x11020000)
+#define RZG2L_GPIO_BASE				(0x11030000)
+#define RZG2L_TZC_SPI_BASE			(0x11060000)
+#define RZG2L_TZC_DDR_BASE			(0x11070000)
+#define RZG2L_DDR_PHY_BASE			(0x11400000)
+#define RZG2L_DDR_MEMC_BASE			(0x11410000)
+#define RZG2L_GIC_BASE				(0x11900000)
+#define RZG2L_SD0_BASE				(0x11C00000)
+#define RZG2L_SPIROM_BASE			(0x20000000)
+#define RZG2L_DDR1_BASE				(0x40000000)
+#define RZG2L_DDR2_BASE				(0x80000000)
+#define RZG2L_DDR3_BASE				(0x100000000)
+
+#define RZG2L_DEVICE_SIZE			(0x15000000 - RZG2L_DEVICE_BASE)
+#define RZG2L_SPIROM_SIZE			(0x30000000 - RZG2L_SPIROM_BASE)
+#define RZG2L_DDR1_SIZE				(RZG2L_DDR2_BASE - RZG2L_DDR1_BASE)
+#define RZG2L_DDR2_SIZE				(RZG2L_DDR3_BASE - RZG2L_DDR2_BASE)
+
+/* TODO , Check the offset */
+#define RZG2L_GICD_BASE				(RZG2L_GIC_BASE + 0x00000000)
+#define RZG2L_GICR_BASE				(RZG2L_GIC_BASE + 0x00000000)
+
+#define RZG2L_SYC_INCK_HZ			(24000000)
+#define RZG2L_UART_INCK_HZ			(100000000)
+#define RZG2L_UART_BARDRATE			(115200)
+
+
+/* plat_gicv3.c */
+void plat_gic_driver_init(void);
+void plat_gic_init(void);
+
+
+
+#endif	/* __RZG2L_PRIVATE_H__ */
