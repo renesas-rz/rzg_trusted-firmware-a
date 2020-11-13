7b2a87b45b80f9805c5572655609652811679820
Takeshi Naruse <takeshi.naruse.te@bp.renesas.com>
2020-11-13 23:28:23 +0900

    Add ddr.h

---
+++ plat/renesas/rzg2l/include/ddr.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _DDR_H_
+#define _DDR_H_
+
+void ddr_setup(void);
+
+#endif	/* _DDR_H_ */
