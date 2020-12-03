0d60a2358745245befbccce550e9352ea73d690b
Takeshi Naruse <takeshi.naruse.te@bp.renesas.com>
2020-12-03 09:18:50 +0900

    Add DDR initialization for FPGA

--- plat/renesas/rzg2l/plat_security.c
+++ plat/renesas/rzg2l/plat_security.c
@@ -7,16 +7,15 @@
 #include <drivers/arm/tzc400.h>
 
 #define TZC400_FILTER_MAX		(4)
-#define TZC400_ID_ACC_REG0		(0x000F000F)
+#define TZC400_PORT_MAX			(4)
+#define TZC400_NSAID_MASK		((1 << TZC400_PORT_MAX) - 1)
 
 void plat_tzc400_setup(uintptr_t tzc_base)
 {
 	tzc400_init(tzc_base);
-//	tzc400_disable_filters();
-	tzc400_configure_region0(TZC_REGION_S_NONE, TZC400_ID_ACC_REG0);
+	tzc400_disable_filters();
+	tzc400_configure_region0(TZC_REGION_S_NONE, 
+		(TZC400_NSAID_MASK << TZC_REGION_ACCESS_WR_EN_SHIFT) | TZC400_NSAID_MASK);
+	tzc400_set_action(TZC_ACTION_ERR);
 	tzc400_enable_filters();
-
-	/* TODO */
-	/* Disables read/write access speculation. */
-	/* tzc400 driver does not support */
 }
