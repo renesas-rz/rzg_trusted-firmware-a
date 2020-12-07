jolivain/trusted-firmware-a

mediatek: mt8192: add uart save and restore api
When system resume, we want to print log as soon as possible.
So we add uart save and restore api, and they will be called
when systtem suspend and resume.

Change-Id: I83b477fd2b39567c9c6b70534ef186993f7053ae
Signed-off-by: Yuchen Huang <yuchen.huang@mediatek.com>
Signed-off-by: Roger Lu <roger.lu@mediatek.com>

plat/mediatek/mt8192/include/platform_def.h
@@ -15,6 +15,7 @@
 #define MCUCFG_BASE        0x0c530000
 
 #define IO_PHYS            0x10000000
+#define INFRACFG_AO_BASE   (IO_PHYS + 0x1000)
 
 /* Aggregate of all devices for MMU mapping */
 #define MTK_DEV_RNG0_BASE    IO_PHYS

renesas-rz/rzg_trusted-firmware-a

plat/mediatek/mt8192/include/platform_def.h
@@ -26,6 +26,7 @@
 #define MTK_MCDI_SRAM_BASE      0x11B000
 #define MTK_MCDI_SRAM_MAP_SIZE  0x1000
 
+#define INFRACFG_AO_BASE (IO_PHYS + 0x00001000)
 #define GPIO_BASE        (IO_PHYS + 0x00005000)
 #define SPM_BASE         (IO_PHYS + 0x00006000)
 #define PMIC_WRAP_BASE   (IO_PHYS + 0x00026000)