e2ef32c00f236818cc61e7fb18cb46a78ce696ec
hiromasa kakutani <hiromasa.kakutani.pv@bp.renesas.com>
2021-03-12 18:03:43 +0900

    Argument check of SPI-Multi initialization process and change of call order in rzg2l_io_setup function

--- plat/renesas/rzg2l/drivers/spi_multi.c
+++ plat/renesas/rzg2l/drivers/spi_multi.c
@@ -72,7 +72,8 @@ int spi_multi_setup(uint32_t addr_width, uint32_t dq_width, uint32_t dummy_cycle
 
 	/* parameter check */
 	if ((addr_width > SPI_MULTI_ADDR_WIDES_32) ||
-		(dq_width > SPI_MULTI_DQ_WIDES_1_4_4) ||
+		(dq_width > SPI_MULTI_DQ_WIDES_1_4_4)  ||
+		(0 == dummy_cycle)                     ||
 		(dummy_cycle > SPI_MULTI_DUMMY_20CYCLE))
 		return -1;
 
