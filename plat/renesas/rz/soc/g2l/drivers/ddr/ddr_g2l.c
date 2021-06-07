7fbbc38f76c048a7214b2119a4dacb4b00c3193f
Takunori <takunori.otsuka.ur@renesas.com>
2021-06-07 17:36:53 +0900

    plat/renesas/rz/soc/g2l/drivers/ddr     Modify the initialization routine for DDR of RZ/G2L.

--- plat/renesas/rz/soc/g2l/drivers/ddr/ddr_g2l.c
+++ plat/renesas/rz/soc/g2l/drivers/ddr/ddr_g2l.c
@@ -6,8 +6,17 @@
 
 #include <ddr_internal.h>
 
+#define	SYS_LSI_DEVID	(0x11020A04)
+
 void ddr_ctrl_reten_en_n(uint8_t val)
 {
 	val &= 1;
-	rmw_phy_reg(DDRPHY_R78, 0xFFFEFFFF, (val << 16));
+	if ((mmio_read_32(SYS_LSI_DEVID) >> 28) == 0)
+	{
+		rmw_phy_reg(DDRPHY_R78, 0xFFFEFFFF, (val << 16));
+	}
+	else
+	{
+		write_phy_reg(DDRPHY_R79, (val << 1));
+	}
 }
