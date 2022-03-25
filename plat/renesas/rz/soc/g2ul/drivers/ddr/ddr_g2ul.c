a9e2e81e71c60c5ae809d81064d3be243fc35e66
Nhan Nguyen <nhan.nguyen.yb@renesas.com>
2022-03-25 15:54:51 +0700

    plat: renesas: rz: bump all support RZ/G2L Group 's driver and boards
    This commit bump all commits from 3fb780632c83ce0^.. e59139eefdcb, on
    branch v2.5/rzg2l, to support all platform and drivers of RZ/G2L,
    RZ/G2LC, RZ/G2UL, RZ/V2L devices.
    
    Signed-off-by: Nhan Nguyen <nhan.nguyen.yb@renesas.com>

---
+++ plat/renesas/rz/soc/g2ul/drivers/ddr/ddr_g2ul.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <ddr_internal.h>
+
+void ddr_ctrl_reten_en_n(uint8_t val)
+{
+	val &= 1;
+	write_phy_reg(DDRPHY_R79, (val << 1));
+}
+
+char *ddr_get_version(void)
+{
+	return AN_VERSION;
+}
