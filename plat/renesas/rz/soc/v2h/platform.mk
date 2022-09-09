1b3605205970158ac3849f99326db41150824b43
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2022-09-09 09:53:34 +0100

    SSGSP2RZBL-17: added template file for RZ/V2H plat_security.c and now building it

--- plat/renesas/rz/soc/v2h/platform.mk
+++ plat/renesas/rz/soc/v2h/platform.mk
@@ -10,11 +10,13 @@ PLAT_INCLUDES	:=	-Iplat/renesas/rz/soc/v2h/include
 include plat/renesas/rz/common/v2h_common.mk
 include plat/renesas/rz/board/${BOARD}/rz_board.mk
 
-BL2_SOURCES		+=	plat/renesas/rz/soc/v2h/bl2_plat_setup.c		\
-					plat/renesas/rz/soc/v2h/drivers/cpg.c			\
-					plat/renesas/rz/soc/v2h/drivers/pfc.c
+PLAT_BL_COMMON_SOURCES	+=	plat/renesas/rz/soc/v2h/plat_security.c
 
-BL31_SOURCES	+=	plat/renesas/rz/soc/v2h/bl31_plat_setup.c		\
-					plat/renesas/rz/soc/v2h/drivers/plat_pm.c
+BL2_SOURCES				+=	plat/renesas/rz/soc/v2h/bl2_plat_setup.c		\
+							plat/renesas/rz/soc/v2h/drivers/cpg.c			\
+							plat/renesas/rz/soc/v2h/drivers/pfc.c
 
-DDR_SOURCES		+=	plat/renesas/rz/soc/v2h/drivers/ddr/ddr_v2h.c
+BL31_SOURCES			+=	plat/renesas/rz/soc/v2h/bl31_plat_setup.c		\
+							plat/renesas/rz/soc/v2h/drivers/plat_pm.c
+
+DDR_SOURCES				+=	plat/renesas/rz/soc/v2h/drivers/ddr/ddr_v2h.c
