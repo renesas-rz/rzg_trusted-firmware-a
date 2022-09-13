52a324789f84cf5d1a78429df66ec4b32effc40d
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2022-09-13 10:07:26 +0100

    SSGSP2RZBL-11: now define SYS_BASE to rz_soc_def.h instead of in  sys_regs.h

--- plat/renesas/rz/soc/v2h/include/sys_regs.h
+++ plat/renesas/rz/soc/v2h/include/sys_regs.h
@@ -7,7 +7,7 @@
 #ifndef __SYS_REGS_H__
 #define __SYS_REGS_H__
 
-#define	SYS_BASE							(0x10430000)
+#include <rz_soc_def.h>						/* Get the System base address */
 
 #define	SYS_MSTACCCTL0						(SYS_BASE + 0x00000000)
 #define	SYS_MSTACCCTL1						(SYS_BASE + 0x00000004)
