fbb8d4cfd56b4634b4cf659f396cb5d93c0da2b6
Bo-Chen Chen <rex-bc.chen@mediatek.com>
2022-09-29 10:41:26 +0800

    feat(mediatek): move lpm drivers back to common
    In order to sync drivers with MediaTek internal code base, we move lpm
    drivers back to common folder.
    
    Signed-off-by: Bo-Chen Chen <rex-bc.chen@mediatek.com>
    Change-Id: I1066e092febe0abb9782a46f668613e137737c88

--- plat/mediatek/mt8186/platform.mk
+++ plat/mediatek/mt8186/platform.mk
@@ -8,6 +8,7 @@ MTK_PLAT     := plat/mediatek
 MTK_PLAT_SOC := ${MTK_PLAT}/${PLAT}
 
 PLAT_INCLUDES := -I${MTK_PLAT}/common/                            \
+                 -I${MTK_PLAT}/common/lpm                         \
                  -I${MTK_PLAT}/drivers/cirq/                      \
                  -I${MTK_PLAT}/drivers/gic600/                    \
                  -I${MTK_PLAT}/drivers/gpio/                      \
