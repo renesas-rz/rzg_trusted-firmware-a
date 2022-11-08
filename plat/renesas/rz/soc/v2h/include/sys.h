3741b89fa43048f267ce203c4292b3467a2c6786
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2022-11-08 20:39:43 +0000

    SSGSP2RZBL-28: added V2H specfic version of sys.h

---
+++ plat/renesas/rz/soc/v2h/include/sys.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __SYS_H__
+#define __SYS_H__
+
+#define MASK_BOOTM_DEVICE		(0x0007)
+#define MASK_BOOTM_SECURE		(0x8000)
+
+#define BOOT_MODE_ESD 			(0)
+#define BOOT_MODE_EMMC_3_3		(1)
+#define BOOT_MODE_SPI_3_3		(2)
+#define BOOT_MODE_SCIF			(3)
+/* Not Used						(4) */
+#define BOOT_MODE_EMMC_1_8		(5)
+#define BOOT_MODE_SPI_1_8		(6)
+/* Not Used						(7) */
+
+#endif	/* __SYS_H__ */
