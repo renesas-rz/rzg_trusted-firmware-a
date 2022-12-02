d9dffd3a4d6e5a13007caaedbb4a9e0eb87050d8
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2022-12-02 11:43:45 +0000

    SSGSP2RZBL-38: added build file plat/renesas/rz/common/g3s_common.mk

---
+++ plat/renesas/rz/common/g3s_common.mk
@@ -0,0 +1,108 @@
+#
+# Copyright (c) 2022, Renesas Electronics Corporation. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+BL2_AT_EL3						:= 1
+COLD_BOOT_SINGLE_CPU			:= 1
+PROGRAMMABLE_RESET_ADDRESS		:= 1
+WARMBOOT_ENABLE_DCACHE_EARLY	:= 1
+GICV3_SUPPORT_GIC600			:= 1
+HW_ASSISTED_COHERENCY			:= 1
+USE_COHERENT_MEM				:= 0
+TRUSTED_BOARD_BOOT				:= 0
+PROTECTED_CHIPID				:= 1
+DEBUG_FPGA						:= 0
+
+$(eval $(call add_define,PROTECTED_CHIPID))
+$(eval $(call add_define,DEBUG_FPGA))
+
+WA_RZG2L_GIC64BIT				:= 1
+$(eval $(call add_define,WA_RZG2L_GIC64BIT))
+
+# Enable workarounds for selected Cortex-A55 erratas.
+ERRATA_A55_768277				:= 1
+ERRATA_A55_778703 				:= 1
+ERRATA_A55_798797 				:= 1
+ERRATA_A55_846532 				:= 1
+ERRATA_A55_903758 				:= 1
+ERRATA_A55_1221012				:= 1
+ERRATA_A55_1530923				:= 1
+
+PLAT_INCLUDES			+=	-Iplat/renesas/rz/common/include								\
+							-Iplat/renesas/rz/common/drivers/emmc							\
+							-Iplat/renesas/rz/common/drivers/io								\
+							-Idrivers/renesas/common/io										\
+							-Iplat/renesas/rz/common/include/drivers/spi_multi
+
+RZ_TIMER_SOURCES		:=	drivers/delay_timer/generic_delay_timer.c			\
+							drivers/delay_timer/delay_timer.c
+
+DDR_SOURCES				:=	plat/renesas/rz/common/drivers/ddr/ddr.c
+
+BL_COMMON_SOURCES		+=	lib/cpus/aarch64/cortex_a55.S						\
+							drivers/arm/tzc/tzc400.c
+
+include lib/xlat_tables_v2/xlat_tables.mk
+PLAT_BL_COMMON_SOURCES	:=	${XLAT_TABLES_LIB_SRCS}								\
+							plat/renesas/rz/common/plat_rz_common.c				\
+							plat/renesas/rz/common/aarch64/plat_helpers.S		\
+							plat/renesas/rz/common/drivers/syc.c				\
+							plat/renesas/rz/common/drivers/scifa.S
+
+ifneq (${ENABLE_STACK_PROTECTOR},0)
+PLAT_BL_COMMON_SOURCES	+=	plat/renesas/rz/common/rz_stack_protector.c
+endif
+
+BL2_SOURCES				+=	common/desc_image_load.c							\
+							drivers/io/io_storage.c								\
+							drivers/io/io_memmap.c								\
+							drivers/io/io_fip.c									\
+							plat/renesas/rz/common/drivers/io/io_emmcdrv.c		\
+							plat/renesas/rz/common/drivers/emmc/emmc_interrupt.c	\
+							plat/renesas/rz/common/drivers/emmc/emmc_utility.c	\
+							plat/renesas/rz/common/drivers/emmc/emmc_mount.c	\
+							plat/renesas/rz/common/drivers/emmc/emmc_init.c		\
+							plat/renesas/rz/common/drivers/emmc/emmc_read.c		\
+							plat/renesas/rz/common/drivers/emmc/emmc_cmd.c		\
+							plat/renesas/rz/common/bl2_plat_mem_params_desc.c	\
+							plat/renesas/rz/common/plat_image_load.c			\
+							plat/renesas/rz/common/plat_storage.c				\
+							${RZ_TIMER_SOURCES}									\
+							${DDR_SOURCES}										\
+							${XSPI_SOURCE}										\
+							${SPI_MULTI_SOURCE}
+
+# Include GICv3 driver files
+GICV3_IMPL				:=	GIC600
+include drivers/arm/gic/v3/gicv3.mk
+
+BL31_SOURCES			:=	plat/common/plat_gicv3.c							\
+							plat/common/plat_psci_common.c						\
+							plat/renesas/rz/common/plat_topology.c				\
+							plat/renesas/rz/common/plat_gic.c					\
+							plat/renesas/rz/common/rz_plat_sip_handler.c		\
+							plat/renesas/rz/common/rz_sip_svc.c					\
+							${GICV3_SOURCES}
+
+ifneq (${TRUSTED_BOARD_BOOT},0)
+
+	# Include common TBB sources
+	AUTH_SOURCES		:=	drivers/auth/img_parser_mod.c
+
+	# Include the selected chain of trust sources.
+	ifeq (${COT},tbbr)
+		AUTH_SOURCES	+=	plat/renesas/rz/common/drivers/auth/tbbr/tbbr_cot.c
+	else
+		$(error Unknown chain of trust ${COT})
+	endif
+
+	# Include RZ TBB sources
+	AUTH_SOURCES		+=	plat/renesas/rz/common/drivers/auth/auth_mod.c				\
+							plat/renesas/rz/common/drivers/auth/sblib/crypto_sblib.c	\
+							plat/renesas/rz/common/drivers/auth/sblib/sblib_parser.c
+
+	BL2_SOURCES		+=	${AUTH_SOURCES}
+
+endif
