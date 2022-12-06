bcaeb9c6c676ccf9bb9d226025827181e9d695de
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2022-12-06 15:26:29 +0000

    SSGSP2RZBL-38: Updated plat_pm.c

--- plat/renesas/rz/soc/g3s/plat_pm.c
+++ plat/renesas/rz/soc/g3s/plat_pm.c
@@ -19,66 +19,44 @@
 #include <common/bl_common.h>
 
 
-#define LO_REG							(0U)
-#define HI_REG							(1U)
-
-
-typedef struct {
-	uintptr_t reg;
-	uint32_t  preq_mask;
-	uint32_t  paccept_mask;
-	uint32_t  pstate_on_mask;
-} CPG_CORE_PWR;
-
-
 uintptr_t	gp_warm_ep;
-
 
 static int rzg3s_pwr_domain_on(u_register_t mpidr)
 {
-	const uint32_t rval[2] = {
-		SYS_ACPU_CFG_RVAL0, SYS_ACPU_CFG_RVAH0,
-	};
-
-	const CPG_CORE_PWR pch = {
-		CPG_LP_CA55_CTL2, CPG_LP_CA55_CTL2_COREPREQ0, CPG_LP_CA55_CTL2_COREACCEPT0, CPG_LP_CA55_CTL2_CORESTATE0_ON_MASK,
-	};
-
 	uint8_t coreid = MPIDR_AFFLVL1_VAL(mpidr);
 
 	if (coreid >= PLATFORM_CORE_COUNT)
 		return PSCI_E_INVALID_PARAMS;
 
-	/* Check if in standby */
-	if ((mmio_read_32(CPG_LP_CTL1) & 0x1) == 0x1) {
-		mmio_write_32(pch.reg, pch.preq_mask);
-		while ((mmio_read_32(pch.reg) & pch.paccept_mask) != pch.paccept_mask)
+	/*  Apply an external reset */
+	if ((mmio_read_32(SYS_LP_CTL2) & 0x1) == 0x1) {
+		mmio_write_32(CPG_CORE0_PCHCTL, 0x00000001);
+		while ((mmio_read_32(CPG_CORE0_PCHMON) & 0x1) != 0x1)
 			;
-		mmio_write_32(pch.reg, 0x00000000);
-		while ((mmio_read_32(pch.reg) & pch.paccept_mask) != 0x0)
+		mmio_write_32(CPG_CORE0_PCHCTL, 0x00000000);
+		while ((mmio_read_32(CPG_CORE0_PCHMON) & 0x1) != 0x0)
 			;
 	}
 
 	/*  Start the core */
-	mmio_write_32(rval[LO_REG], (uint32_t)(gp_warm_ep & 0xFFFFFFFC));
-	mmio_write_32(rval[HI_REG], (uint32_t)((gp_warm_ep >> 32) & 0xFF));
+	mmio_write_32(SYS_CA55_CFG_RVAL0, (uint32_t)(gp_warm_ep & 0xFFFFFFFC));
+	mmio_write_32(SYS_CA55_CFG_RVAH0, (uint32_t)((gp_warm_ep >> 32) & 0xFF));
 
 	/* Assert PORESET */
-	mmio_write_32(CPG_RST_0, (0x00010000));
-	while ((mmio_read_32(CPG_RSTMON_0) & (0x1)) == 0x0)
+	mmio_write_32(CPG_RST_CA55, 0x00010000);
+	while ((mmio_read_32(CPG_RSTMON_CA55) & 0x1) == 0x0)
 		;
 
-	/* Deassert PORESET and RERESET */
-	mmio_write_32(CPG_RST_0, (0x00110011));
-	while ((mmio_read_32(CPG_RSTMON_0) & (0x1)) != 0x0)
+	/* Deassert PORESET */
+	mmio_write_32(CPG_RST_CA55, 0x00050005);
+	while ((mmio_read_32(CPG_RSTMON_CA55) & 0x1) != 0x0)
 		;
 
-	mmio_write_32(pch.reg, (pch.pstate_on_mask | pch.preq_mask));
-	while ((mmio_read_32(pch.reg) & pch.paccept_mask) != pch.paccept_mask)
+	mmio_write_32(CPG_CORE0_PCHCTL, 0x00080001);
+	while ((mmio_read_32(CPG_CORE0_PCHMON) & 0x1) != 0x1)
 		;
-
-	mmio_write_32(pch.reg, pch.pstate_on_mask);
-	while ((mmio_read_32(pch.reg) & pch.paccept_mask) != 0x0)
+	mmio_write_32(CPG_CORE0_PCHCTL, 0x00080000);
+	while ((mmio_read_32(CPG_CORE0_PCHMON) & 0x1) != 0x0)
 		;
 
 	return PSCI_E_SUCCESS;
@@ -103,12 +81,18 @@ static void rzg3s_pwr_domain_off(const psci_power_state_t *state)
 	/* Prevent interrupts from spuriously waking up this cpu */
 	plat_gic_cpuif_disable();
 
-	/* Request transition to Cortex-A55 CoreX Sleep Mode */
-	mmio_write_32(CPG_LP_CTL1, CPG_LP_CTL1_CA55SLEEP_REQ);
+	/*  Enable the transition request interrupt to the Cortex-A55 Sleep Mode */
+	mmio_write_32(SYS_LP_CTL6, 0x00000100);
+
+	/* Transition request to Cortex-A55 CoreX Sleep Mode */
+	mmio_write_32(SYS_LP_CTL1, 0x00000100);
 
 	/* Confirm that the processing on the Cortex-M33 side is completed */
-	while ((mmio_read_32(CPG_LP_CTL1) & CPG_LP_CTL1_CA55SLEEP_ACK) != CPG_LP_CTL1_CA55SLEEP_ACK)
+	while ((mmio_read_32(SYS_LP_CTL5) & 0x00000100) != 0x00000100)
 		;
+	/* Enter the Cortex-A55 Sleep Mode */
+	/* Start the Cortex-A55 Sleep Mode */
+	mmio_write_32(SYS_LP_CTL2, 0x00000001);
 
 	/* Issue Barrier instruction */
 	isb();
