f6eb1db167f3f45b4700e92c38b21666c276afcf
MichaelAllportWM <michael.allport.wm@renesas.com>
2022-12-10 13:39:55 +0000

    pfc.c changed a number of values. Added pfc_drive_setup() which sets the IOLH register to drive the relevant pins. Removed the qspi references and used xspi instead. pfc_regs.h changed a table name to reflect the xspi.

--- plat/renesas/rz/soc/g3s/drivers/pfc.c
+++ plat/renesas/rz/soc/g3s/drivers/pfc.c
@@ -7,21 +7,19 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <pfc_regs.h>
+#include <sys_regs.h>
 #include <lib/mmio.h>
 
 //TODO: Check Slew Rate registers exist and their values are the same as the G2L.
 //TODO: SD1 and SD0 both required? ESD and EMC different ports?
-//TODO: Check IOLH drive level. G2L values used.
-
-//64 vs 32 bit registers
-//1.8 vs 3.3?
+//TODO: Check IOLH drive level. V2H values used.
 
 static PFC_REGS pfc_mux_sd_reg_tbl[PFC_MUX_SD_TBL_NUM] = {
 	/* P0(sd0) CP, WP - (sd1) CP, WP */
 	{
 		{ PFC_ON,	(uintptr_t)PFC_PMC20,	0x0F },					/* PMC */
 		{ PFC_ON,	(uintptr_t)PFC_PFC20,	0x00001111 },			/* PFC */
-		{ PFC_OFF,	(uintptr_t)PFC_IOLH20,	0x0000000001010101 },	/* IOLH */
+		{ PFC_OFF,	(uintptr_t)PFC_IOLH20,	0x0000000003030303 },	/* IOLH */
 		{ PFC_OFF,	(uintptr_t)PFC_PUPD20,	0x0000000000000000 },	/* PUPD */
 		{ PFC_OFF, 	(uintptr_t)PFC_SR20,	0x0000000001010101 },	/* SR */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 }						/* IEN */
@@ -34,30 +32,30 @@ static PFC_REGS pfc_mux_scif_reg_tbl[PFC_MUX_SD_TBL_NUM] = {
 	{
 		{ PFC_ON,	(uintptr_t)PFC_PMC25,	0x1F },					/* PMC */
 		{ PFC_ON,	(uintptr_t)PFC_PFC25,	0x00011111 },			/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH25,	0x0000000101010101 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH25,	0x0000000303030303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD25,	0x0000000000000000 },	/* PUPD */
 		{ PFC_ON,	(uintptr_t)PFC_SR25,	0x0000000101010101 },	/* SR */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 }						/* IEN */
 	}
 };
 
-static PFC_REGS  pfc_qspi_reg_tbl[PFC_QSPI_TBL_NUM] = {
-	/* XSPI  - CS1, CS0, DS, WP, RESET */
+static PFC_REGS  pfc_xspi_reg_tbl[PFC_XSPI_TBL_NUM] = {
+	/* XSPI  - CS1, CS0, DS, WP, RESET, SPCLK */
 	{
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PMC */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH04,	0x0000000000020202 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH04,	0x0000030303030303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD04,	0x0000000000000000 },	/* PUPD */
-		{ PFC_ON,	(uintptr_t)PFC_SR04,	0x0000000000010000 },	/* SR */
+		{ PFC_ON,	(uintptr_t)PFC_SR04,	0x0000010101010101 },	/* SR */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 }						/* IEN */
 	},
 	/* XSPI - IO7 - IO0*/
 	{
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PMC */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH05,	0x0000020202020202 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH05,	0x0303030303030303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD05,	0x0000000000000000 },	/* PUPD */
-		{ PFC_ON,	(uintptr_t)PFC_SR05,	0x0000010101010101 },	/* SR */
+		{ PFC_ON,	(uintptr_t)PFC_SR05,	0x0101010101010101 },	/* SR */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 }						/* IEN */
 	}
 };
@@ -67,7 +65,7 @@ static PFC_REGS  pfc_sd_reg_tbl[PFC_SD_TBL_NUM] = {
 	{
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PMC */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH10,	0x0000000000020202 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH10,	0x0000000000030303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD10,	0x0000000000000000 },	/* PUPD */
 		{ PFC_ON,	(uintptr_t)PFC_SR10,	0x0000000000010101 },	/* SR */
 		{ PFC_ON,	(uintptr_t)PFC_IEN10,	0x0000000000000100 }	/* IEN */
@@ -76,7 +74,7 @@ static PFC_REGS  pfc_sd_reg_tbl[PFC_SD_TBL_NUM] = {
 	{
 		{ PFC_OFF, (uintptr_t)NULL,			0 },					/* PMC */
 		{ PFC_OFF, (uintptr_t)NULL,			0 },					/* PFC */
-		{ PFC_ON,  (uintptr_t)PFC_IOLH11,	0x0202020202020202 },	/* IOLH */
+		{ PFC_ON,  (uintptr_t)PFC_IOLH11,	0x0303030303030303 },	/* IOLH */
 		{ PFC_ON,  (uintptr_t)PFC_PUPD11,	0x0000000000000000 },	/* PUPD */
 		{ PFC_ON,  (uintptr_t)PFC_SR11,		0x0101010101010101 },	/* SR */
 		{ PFC_ON,  (uintptr_t)PFC_IEN11,	0x0101010101010101 }	/* IEN */
@@ -85,19 +83,19 @@ static PFC_REGS  pfc_sd_reg_tbl[PFC_SD_TBL_NUM] = {
 	{
 		{ PFC_OFF,	(uintptr_t)NULL, 		0 },					/* PMC */
 		{ PFC_OFF,	(uintptr_t)NULL, 		0 },					/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH12,	0x0000000000000202 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH12,	0x0000000000000303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD12,	0x0000000000000000 },	/* PUPD */
 		{ PFC_ON,	(uintptr_t)PFC_SR12,	0x0000000000000101 },	/* SR */
 		{ PFC_ON,	(uintptr_t)PFC_IEN12,	0x0000000000000100 }	/* IEN */
 	},
-	/* SD1 DATA7 - DATA0 */
+	/* SD1 DATA3 - DATA0 */
 	{
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PMC */
 		{ PFC_OFF,	(uintptr_t)NULL,		0 },					/* PFC */
-		{ PFC_ON,	(uintptr_t)PFC_IOLH13,	0x0202020202020202 },	/* IOLH */
+		{ PFC_ON,	(uintptr_t)PFC_IOLH13,	0x0000000003030303 },	/* IOLH */
 		{ PFC_ON,	(uintptr_t)PFC_PUPD13,	0x0000000000000000 },	/* PUPD */
-		{ PFC_ON,	(uintptr_t)PFC_SR13,	0x0101010101010101 },	/* SR */
-		{ PFC_ON,	(uintptr_t)PFC_IEN13,	0x0101010101010101 }	/* IEN */
+		{ PFC_ON,	(uintptr_t)PFC_SR13,	0x0000000001010101 },	/* SR */
+		{ PFC_ON,	(uintptr_t)PFC_IEN13,	0x0000000001010101 }	/* IEN */
 	}
 };
 
@@ -110,22 +108,22 @@ static void pfc_write_registers(uint8_t tbl_size, PFC_REGS* pfc_reg_tbl)
 		if (pfc_reg_tbl[cnt].pmc.flg == PFC_ON) {
 			mmio_write_8(pfc_reg_tbl[cnt].pmc.reg, pfc_reg_tbl[cnt].pmc.val);
 		}
+
 		/* PFC */
 		if (pfc_reg_tbl[cnt].pfc.flg == PFC_ON) {
 			mmio_write_32(pfc_reg_tbl[cnt].pfc.reg, pfc_reg_tbl[cnt].pfc.val);
 		}
-		/* IOLH */
-		if (pfc_reg_tbl[cnt].iolh.flg == PFC_ON) {
-			mmio_write_64(pfc_reg_tbl[cnt].iolh.reg, pfc_reg_tbl[cnt].iolh.val);
-		}
+
 		/* PUPD */
 		if (pfc_reg_tbl[cnt].pupd.flg == PFC_ON) {
 			mmio_write_64(pfc_reg_tbl[cnt].pupd.reg, pfc_reg_tbl[cnt].pupd.val);
 		}
+
 		/* SR */
 		if (pfc_reg_tbl[cnt].sr.flg == PFC_ON) {
 			mmio_write_64(pfc_reg_tbl[cnt].sr.reg, pfc_reg_tbl[cnt].sr.val);
 		}
+
 		/* IEN */
 		if (pfc_reg_tbl[cnt].ien.flg == PFC_ON) {
 			mmio_write_64(pfc_reg_tbl[cnt].ien.reg, pfc_reg_tbl[cnt].ien.val);
@@ -145,9 +143,9 @@ static void pfc_scif_setup(void)
 	mmio_write_32(PFC_PWPR, PWPR_B0Wl);
 }
 
-static void pfc_qspi_setup(void)
+static void pfc_xspi_setup(void)
 {
-	pfc_write_registers(PFC_QSPI_TBL_NUM, pfc_qspi_reg_tbl);
+	pfc_write_registers(PFC_XSPI_TBL_NUM, pfc_xspi_reg_tbl);
 }
 
 static void pfc_sd_setup(void)
@@ -168,9 +166,57 @@ static void pfc_sd_setup(void)
 	mmio_write_32(PFC_PWPR, PWPR_B0Wl);
 }
 
+static const PFC_REGS *pfc_boot_mode_tbls[SYS_LSI_MODE_COUNT] = {
+	pfc_sd_reg_tbl,
+	pfc_sd_reg_tbl,
+	pfc_sd_reg_tbl,
+	pfc_xspi_reg_tbl,
+	pfc_xspi_reg_tbl,
+	pfc_mux_scif_reg_tbl
+};
+
+static const uint8_t pfc_boot_mode_tbl_len[SYS_LSI_MODE_COUNT] = {
+	PFC_SD_TBL_NUM,
+	PFC_SD_TBL_NUM,
+	PFC_SD_TBL_NUM,
+	PFC_XSPI_TBL_NUM,
+	PFC_XSPI_TBL_NUM,
+	PFC_MUX_SCIF_TBL_NUM
+};
+
+static void pfc_drive_setup(void)
+{
+	static const uint64_t pfc_iolh_drive_tbl[4] = {0x0000000000000000, 0x0101010101010101, 0x0202020202020202, 0x0303030303030303};
+	/* Get the boot mode */
+	uint16_t boot_mode = *((uint16_t *)RZ_SOC_BOOTINFO_BASE);
+
+	if (boot_mode < SYS_LSI_MODE_COUNT) {
+		const PFC_REGS *p_pins_tbl = pfc_boot_mode_tbls[boot_mode];
+		uint8_t tbl_len = pfc_boot_mode_tbl_len[boot_mode];
+
+		uint32_t sys_lsi_otppoc = mmio_read_32(SYS_LSI_OTPPOC);
+		uint64_t pfc_iolh_drive = 0;
+		int cnt;
+
+		if (0 != (sys_lsi_otppoc & (SYS_LSI_OTPPOC_EN_x_DS_MASK << (SYS_LSI_OTPPOC_EN_x_DS_BASE + (SYS_LSI_OTPPOC_EN_x_DS_WIDTH * boot_mode))))) {
+			uint32_t index = sys_lsi_otppoc & ((SYS_LSI_OTPPOC_x_E_MASK << (SYS_LSI_OTPPOC_x_E_WIDTH * boot_mode)) >> (SYS_LSI_OTPPOC_x_E_WIDTH * boot_mode));
+
+			pfc_iolh_drive = pfc_iolh_drive_tbl[index];
+		}
+
+		for (cnt = 0; cnt < tbl_len; cnt++) {
+			if (p_pins_tbl[cnt].iolh.flg == PFC_ON) {
+				/* Write IOLH value from pfc_sd_reg_tbl[] masked with value in pin table */
+				mmio_write_64(p_pins_tbl[cnt].iolh.reg, (pfc_iolh_drive & p_pins_tbl[cnt].iolh.val));
+			}
+		}
+	}
+}
+
 void pfc_setup(void)
 {
 	pfc_scif_setup();
-	pfc_qspi_setup();
+	pfc_xspi_setup();
 	pfc_sd_setup();
+	pfc_drive_setup();
 }
