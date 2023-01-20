9177f57a76ae6f705123d6eb9b3ebb72ac713338
Takunori Otsuka <takunori.otsuka.ur@renesas.com>
2023-01-20 18:55:46 +0900

    Add writing function to xSPI driver

--- plat/renesas/rz/common/drivers/xspi.c
+++ plat/renesas/rz/common/drivers/xspi.c
@@ -11,28 +11,31 @@
 #include <xspi_regs.h>
 #include <xspi.h>
 
+#define XSPI_BMCFG_SET_VALUE		(0x00010001UL)
+#define XSPI_CMCFG0CS0_SET_VALUE	(0x00000008UL)
+#define XSPI_CMCFG1CS0_SET_VALUE	(0x00000300UL)
+#define XSPI_CMCFG2CS0_SET_VALUE	(0x00000200UL)
+#define XSPI_LIOCFGCS0_SET_VALUE	(0x00070000UL)
+#define XSPI_BMCTL0_SET_VALUE		(0x00000003UL)
+#define XSPI_CDTBUF0_SET_VALUE		(0x00000000UL)
+#define XSPI_INTC_SET_VALUE			(0x00000001UL)
 
-#define XSPI_BMCFG_SET_VALUE				(0x00010001UL)
-#define XSPI_CMCFG0CS0_SET_VALUE			(0x00080008UL)
-#define XSPI_CMCFG1CS0_SET_VALUE			(0x00000300UL)
-#define XSPI_CMCFG2CS0_SET_VALUE			(0x00000200UL)
-#define XSPI_LIOCFGCS0_SET_VALUE			(0x00000000UL)
-#define XSPI_BMCTL0_SET_VALUE				(0x00000003UL)
-#define XSPI_CDTBUF0_SET_VALUE				(0x00000000UL)
-#define XSPI_INTC_SET_VALUE					(0x00000000UL)
+#define XSPI_IN						(0u)
+#define XSPI_OUT					(1u)
 
-#define XSPI_IN					(0u)
-#define XSPI_OUT				(1u)
+#define RDID						(0)
+#define RSTEN						(1)
+#define RESET						(2)
+#define WTEN						(3)
+#define ERASE						(4)
+#define RDSTA						(5)
+#define WRITE						(6)
 
-#define MX25_RDID				(0x00u)
-#define MX25_RSTEN				(0x01u)
-#define MX25_RST				(0x02u)
+#define DEVID_ID_MASK				(0x00FFFFFFu)
+#define DEVICE_ID_BAD				(0u)
+#define DEVICE_ID_ERROR				(0x00FFFFFFu)
 
-#define DEVID_ID_MASK			(0x00FFFFFFu)
-#define DEVICE_ID_BAD			(0u)
-#define DEVICE_ID_ERROR			(0x00FFFFFFu)
-
-#define XSPI_COMMAND_TIMEOUT	(100000u)
+#define XSPI_COMMAND_TIMEOUT		(100000u)
 
 
 typedef struct {
@@ -50,13 +53,15 @@ typedef struct {
 	uint32_t data;
 } st_xspi_cmd_info_t;
 
-static st_xspi_cmd_info_t m_cmd_info;
-
-static const st_xspi_cmd_t mx25_cmds[] = {
-/* {instruction,   direction,	latency,	 data_size,	 addr_size,	 cmd_size}*/
-	{0x9F00u,		XSPI_IN,	 0u,		   3u,		   0u,		  1u},	/* MX25_RDID */
-	{0x6600u,		XSPI_OUT,	 0u,		   0u,		   0u,		  1u},	/* MX25_RSTEN */
-	{0x9900u,		XSPI_OUT,	 0u,		   0u,		   0u,		  1u},	/* MX25_RST */
+static const st_xspi_cmd_t cmds[] = {
+/* {instruction,   direction,	latency,	data_size,	addr_size,	cmd_size}*/
+	{0x9F00u,		XSPI_IN,	0u,			3u,			0u,			1u},	/* RDID */
+	{0x6600u,		XSPI_OUT,	0u,			0u,			0u,			1u},	/* RSTEN */
+	{0x9900u,		XSPI_OUT,	0u,			0u,			0u,			1u},	/* RESET */
+	{0x0600u,		XSPI_OUT,	0u,			0u,			0u,			1u},	/* WTEN */
+	{0x2000u,		XSPI_OUT,	0u,			0u,			3u,			1u},	/* ERASE */
+	{0x0500u,		XSPI_IN,	0u,			1u,			0u,			1u},	/* RDSTA */
+	{0x0200u,		XSPI_OUT,	0u,			4u,			3u,			1u},	/* WRITE */
 };
 
 static int xspi_single_command(const st_xspi_cmd_info_t * const p_cmd_info)
@@ -65,15 +70,18 @@ static int xspi_single_command(const st_xspi_cmd_info_t * const p_cmd_info)
 
 	mmio_write_32(XSPI_CDCTL0, mmio_read_32(XSPI_CDCTL0) & (~XSPI_CDCTL0_TRREQ_MSK));
 
-	mmio_write_32(XSPI_CDTBUF0,	  (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].instruction) << XSPI_CDTBUF_CMD_OFFSET)
-								| (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].direction) << XSPI_CDTBUF_TRTYPE_OFFSET)
-								| (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].latency)	<< XSPI_CDTBUF_LATE_OFFSET)
-								| (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].data_size) << XSPI_CDTBUF_DATASIZE_OFFSET)
-								| (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].addr_size) << XSPI_CDTBUF_ADDSIZE_OFFSET)
-								| (((uint32_t)mx25_cmds[p_cmd_info->cmd_idx].inst_size) << XSPI_CDTBUF_CMDSIZE_OFFSET));
+	mmio_write_32(XSPI_CDTBUF0,	  (((uint32_t)cmds[p_cmd_info->cmd_idx].instruction) << XSPI_CDTBUF_CMD_OFFSET)
+								| (((uint32_t)cmds[p_cmd_info->cmd_idx].direction)   << XSPI_CDTBUF_TRTYPE_OFFSET)
+								| (((uint32_t)cmds[p_cmd_info->cmd_idx].latency)	 << XSPI_CDTBUF_LATE_OFFSET)
+								| (((uint32_t)cmds[p_cmd_info->cmd_idx].data_size)   << XSPI_CDTBUF_DATASIZE_OFFSET)
+								| (((uint32_t)cmds[p_cmd_info->cmd_idx].addr_size)   << XSPI_CDTBUF_ADDSIZE_OFFSET)
+								| (((uint32_t)cmds[p_cmd_info->cmd_idx].inst_size)   << XSPI_CDTBUF_CMDSIZE_OFFSET));
 
 	mmio_write_32(XSPI_CDABUF0, p_cmd_info->addr);
-	mmio_write_32(XSPI_CDD0BUF0, p_cmd_info->data);
+
+	if (XSPI_OUT == cmds[p_cmd_info->cmd_idx].direction) {
+		mmio_write_32(XSPI_CDD0BUF0, p_cmd_info->data);
+	}
 
 	mmio_write_32(XSPI_CDCTL0, mmio_read_32(XSPI_CDCTL0) | XSPI_CDCTL0_TRREQ_MSK);
 
@@ -95,14 +103,12 @@ static int xspi_reset(void)
 	int ret;
 
 	/* Issue the reset command */
-	m_cmd_info.cmd_idx = MX25_RSTEN;
-	m_cmd_info.data = 0;
-	m_cmd_info.addr = 0;
-
-	ret = xspi_single_command(&m_cmd_info);
+	st_xspi_cmd_info_t cmd_rsten = {RSTEN, 0, 0};
+	st_xspi_cmd_info_t cmd_reset = {RESET, 0, 0};
+	
+	ret = xspi_single_command(&cmd_rsten);
 	if (ret == XSPI_SUCCESS) {
-		m_cmd_info.cmd_idx = MX25_RST;
-		ret = xspi_single_command(&m_cmd_info);
+		ret = xspi_single_command(&cmd_reset);
 	}
 
 	return ret;
@@ -112,14 +118,12 @@ static int xspi_read_identification(void)
 {
 	uint32_t id = DEVICE_ID_BAD;
 	uint32_t prev_id = DEVICE_ID_BAD;
-	uint32_t count = 1000;
+	int32_t count = 1000;
 
-	m_cmd_info.cmd_idx = MX25_RDID;
-	m_cmd_info.data = 0;
-	m_cmd_info.addr = 0;
+	st_xspi_cmd_info_t cmd_rdid = {RDID, 0, 0};
 
 	while (count > 0U) {
-		if (xspi_single_command(&m_cmd_info) == XSPI_SUCCESS) {
+		if (xspi_single_command(&cmd_rdid) == XSPI_SUCCESS) {
 			/* Command success */
 			id = mmio_read_32(XSPI_CDD0BUF0) & DEVID_ID_MASK;
 			if ((id != DEVICE_ID_BAD) && (id != DEVICE_ID_ERROR) && (prev_id == id)) {
@@ -136,6 +140,96 @@ static int xspi_read_identification(void)
 	return id;
 }
 
+static int xspi_read_status(void)
+{
+	volatile uint32_t status = 0xFFFFFFFF;
+
+	st_xspi_cmd_info_t cmd_rdsta = {RDSTA, 0, 0};
+
+	if (XSPI_SUCCESS == xspi_single_command(&cmd_rdsta)) {
+		/* Command success */
+		status = mmio_read_32(XSPI_CDD0BUF0);
+	}
+
+	return status;
+}
+
+int xspi_erase(const uintptr_t addr, uint32_t byte_count)
+{
+	int ret = XSPI_SUCCESS;
+
+	uint32_t count = div_round_up(byte_count, XSPI_WRITE_PROG_SIZE);
+
+	st_xspi_cmd_info_t cmd_wten = {WTEN, 0, 0};
+	st_xspi_cmd_info_t cmd_erase = {ERASE, addr, 0};
+
+	volatile uint32_t status = 0xFFFFFFFF;
+
+	for (int i = 0; i < count; i++) {
+
+		ret = xspi_single_command(&cmd_wten);
+		if (XSPI_SUCCESS != ret)
+			return ret;
+
+		do {
+			status = xspi_read_status();
+		} while (0 == (status & 0x02));
+
+		ret = xspi_single_command(&cmd_erase);
+		if (XSPI_SUCCESS != ret)
+			return ret;
+
+		do {
+			status = xspi_read_status();
+		} while (0 != (status & 0x01));
+
+		cmd_erase.addr = cmd_erase.addr + 0x1000;
+	}
+
+	return ret;
+}
+
+int xspi_write(const uintptr_t addr, uintptr_t data, uint32_t byte_count)
+{
+	int ret = XSPI_SUCCESS;
+
+	uint32_t *src = (uint32_t *)data;
+	uint32_t count = byte_count / sizeof(uint32_t);
+
+	st_xspi_cmd_info_t cmd_wten = {WTEN, 0, 0};
+	st_xspi_cmd_info_t cmd_write = {WRITE, addr, 0};
+
+	volatile uint32_t status = 0xFFFFFFFF;
+
+	ret = xspi_erase(addr, byte_count);
+	if (XSPI_SUCCESS != ret)
+		return ret;
+
+	for (int i = 0; i < count; i++) {
+
+		ret = xspi_single_command(&cmd_wten);
+		if (XSPI_SUCCESS != ret)
+			return ret;
+
+		do {
+			status = xspi_read_status();
+		} while (0 == (status & 0x02));
+
+		cmd_write.data = src[i];
+		ret = xspi_single_command(&cmd_write);
+		if (XSPI_SUCCESS != ret)
+			return ret;
+
+		do {
+			status = xspi_read_status();
+		} while (0 != (status & 0x01));
+
+		cmd_write.addr += sizeof(uint32_t);
+	}
+
+	return ret;
+}
+
 int xspi_setup(void)
 {
 	int ret;
@@ -146,10 +240,10 @@ int xspi_setup(void)
 	mmio_write_32(XSPI_CMCFG2CS0,	XSPI_CMCFG2CS0_SET_VALUE);
 	mmio_write_32(XSPI_LIOCFGCS0,	XSPI_LIOCFGCS0_SET_VALUE);
 	mmio_write_32(XSPI_BMCTL0,		XSPI_BMCTL0_SET_VALUE);
-	mmio_write_32(XSPI_CDTBUF0,		XSPI_CDTBUF0_SET_VALUE);
-	mmio_write_32(XSPI_INTC,		XSPI_BMCFG_SET_VALUE);
+	mmio_write_32(XSPI_INTC,		XSPI_INTC_SET_VALUE);
 
 	ret = xspi_reset();
+
 	if (ret == XSPI_SUCCESS)
 		ret = xspi_read_identification();
 
--- plat/renesas/rz/common/include/xspi.h
+++ plat/renesas/rz/common/include/xspi.h
@@ -11,6 +11,11 @@
 #define XSPI_SUCCESS				(0)
 #define XSPI_ERROR					(-1)
 
+#define XSPI_WRITE_SIZE_SHIFT		(12)	/* 4k bytes */
+#define XSPI_WRITE_PROG_SIZE		(1 << XSPI_WRITE_SIZE_SHIFT) /* 2^N bytes */
+
 int xspi_setup(void);
+int xspi_erase(const uintptr_t addr, uint32_t byte_count);
+int xspi_write(const uintptr_t addr, uintptr_t data, uint32_t byte_count);
 
 #endif	/* _XSPI_H_ */
