67e53dc59457ed953ae8ddd63a4698b1faa795e2
Takunori Otsuka <takunori.otsuka.ur@renesas.com>
2023-02-27 13:40:34 +0900

    fix typo

--- plat/renesas/rz/soc/g3s/drivers/ddr/ddr.c
+++ plat/renesas/rz/soc/g3s/drivers/ddr/ddr.c
@@ -75,7 +75,7 @@ void ddr_retention_entry(void)
 	dwc_ddrphy_apb_poll(0x0006E0fa, 1 << 0, 1 << 0);
 	/* PwrOkIn = 0 */
 	mmio_write_32(CPG_RST_DDR, 0x01000000);
-	/* DDRPHY Power Donw Ctrl */
+	/* DDRPHY Power Down Ctrl */
 	mmio_write_32(PWRDN_DDRPHY_CTRL, 0x00000311);
 	/* Wait 18 DfiClk */
 	wait_dficlk(18);
@@ -84,7 +84,7 @@ void ddr_retention_entry(void)
 void ddr_retention_exit(void)
 {
 	INFO("DDR: Retention Exit (Rev. %s)\n", DDR_VERSION);
-	/* DDRPHY Power Donw Ctrl */
+	/* DDRPHY Power Down Ctrl */
 	mmio_write_32(PWRDN_DDRPHY_CTRL, 0x00000200);
 	INFO("DDR: Activate CPG1\n");
 	cpg_active_ddr1();
