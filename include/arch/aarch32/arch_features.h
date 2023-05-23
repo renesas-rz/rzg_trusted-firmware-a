6b094794a648c89cd01d262175d2a091d38712ac
Andre Przywara <andre.przywara@arm.com>
2023-05-23 10:34:38 +0100

    feat(cpufeat): deny AArch64-only features when building for AArch32
    Many newer architecture features are defined for AArch64 only, so cannot
    be used in an AArch32 build.
    
    To avoid #ifdef-ing every single user, just provide trivial
    implementations of the feature check functions is_feat_xxx_supported(),
    which always return "false" in AArch32. The compiler will then optimise
    out the dependent code automatically.
    
    Change-Id: I1e7d653fca0e676a11858efd953c2d623f2d5c9e
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- include/arch/aarch32/arch_features.h
+++ include/arch/aarch32/arch_features.h
@@ -187,22 +187,4 @@ static inline bool is_feat_mtpmu_supported(void)
 	return mtpmu != 0U && mtpmu != ID_DFR1_MTPMU_DISABLED;
 }
 
-static inline unsigned int read_feat_pan_id_field(void)
-{
-	return ISOLATE_FIELD(read_id_mmfr3(), ID_MMFR3_PAN);
-}
-
-static inline bool is_feat_pan_supported(void)
-{
-	if (ENABLE_FEAT_PAN == FEAT_STATE_DISABLED) {
-		return false;
-	}
-
-	if (ENABLE_FEAT_PAN == FEAT_STATE_ALWAYS) {
-		return true;
-	}
-
-	return read_feat_pan_id_field() != 0U;
-}
-
 #endif /* ARCH_FEATURES_H */
