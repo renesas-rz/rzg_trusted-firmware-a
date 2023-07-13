ba1c37b147991db61c0db59bd50aae00438fbfff
Matt Schulte <matsch@google.com>
2023-07-13 11:10:32 -0700

    feat(libc): add %X to printf/snprintf
    Enables printing captial hex chars as well as lowercase
    
    Change-Id: I4dc48c3db97b908f0bb344d7765807967de8cf02
    Signed-off-by: Matt Schulte <matsch@google.com>

--- lib/libc/printf.c
+++ lib/libc/printf.c
@@ -140,10 +140,6 @@ loop:
 				(void)putchar(va_arg(args, int));
 				count++;
 				break;
-			case 'c':
-				(void)putchar(va_arg(args, int));
-				count++;
-				break;
 			case 's':
 				str = va_arg(args, char *);
 				count += string_print(str);
