022b3b03bbadf321cc3847d15bff045a4238175e
MichaelAllportWM <michael.allport.wm@renesas.com>
2023-07-17 12:05:46 +0100

    fixed spelling mistake

--- plat/renesas/rz/soc/g3s/plat_ddr_setup.c
+++ plat/renesas/rz/soc/g3s/plat_ddr_setup.c
@@ -63,7 +63,7 @@ static int save_ddr_config(unsigned int image_id, image_info_t *image_data)
 		return io_result;
 	}
 
-	INFO("Saving DDR retantion info.\n");
+	INFO("Saving DDR retention info.\n");
 
 	io_result = io_size(image_handle, &image_size);
 	if ((io_result != 0) || (image_size == 0U)) {
@@ -88,7 +88,7 @@ static int save_ddr_config(unsigned int image_id, image_info_t *image_data)
 		goto exit;
 	}
 
-	INFO("DDR Retantion Info saved.\n");
+	INFO("DDR Retention Info saved.\n");
 
 exit:
 	(void)io_close(image_handle);
