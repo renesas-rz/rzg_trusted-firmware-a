0fefd09e1ca65ce5c6ae692bf4889a06209659c0
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2023-07-19 10:39:08 +0100

    feat(auth): add explicit entries for key OIDs
    Key-OIDs that authenticate BL31, BL31(SOC)-FW config, and HW config
    images have been explicitly entered.
    Implementations of signer-ID consume these entries.
    
    Change-Id: I24c9085ed5f266af06d40fb73302e35d857a9d5b
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- include/tools_share/cca_oid.h
+++ include/tools_share/cca_oid.h
@@ -9,7 +9,6 @@
 
 /* Reuse the Object IDs defined by TBBR for certificate extensions. */
 #include "tbbr_oid.h"
-#include "zero_oid.h"
 
 /*
  * Assign arbitrary Object ID values that do not conflict with any of the
