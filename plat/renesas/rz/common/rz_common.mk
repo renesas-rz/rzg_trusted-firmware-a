df414c2a6a7ce1c2eec1c55b4deaf4b0af4139b6
Hien Huynh <hien.huynh.px@renesas.com>
2023-09-12 07:38:11 +0700

    plat: renesas: rz: common: add ECC support for RZ/G2L Series
    Add ECC support for RZ/G2L Series by updating DDR setup flow.
    To enable ECC, DDR_ECC_ENABLE should be set to 1.
    DDR_ECC_DETECT or DDR_ECC_DETECT_CORRECT should be set to 1 if
    enable detection mode or detection and correction mode.
    
    Signed-off-by: NAQIN LI <naqin.li.nx@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- plat/renesas/rz/common/rz_common.mk
+++ plat/renesas/rz/common/rz_common.mk
@@ -34,6 +34,14 @@ override CRYPTO_SUPPORT			:= 0
 # Enable workarounds for selected Cortex-A55 erratas.
 ERRATA_A55_1530923				:= 1
 
+# DDR ECC support
+DDR_ECC_ENABLE					:= 0
+DDR_ECC_DETECT					:= 0
+DDR_ECC_DETECT_CORRECT			:= 0
+$(eval $(call add_define,DDR_ECC_ENABLE))
+$(eval $(call add_define,DDR_ECC_DETECT))
+$(eval $(call add_define,DDR_ECC_DETECT_CORRECT))
+
 ifndef SPI_FLASH
   SPI_FLASH = MT25QU512ABB
 endif
