2c92deb1bf1a000122551d2407057d9f50b75257
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2023-11-30 14:30:53 +0000

    SSGSP2RZBL-138: V2H: updated 'evk_alpha' to align with DDR APN Rev1.02 changes

--- plat/renesas/rz/board/v2h_evk_alpha/rz_board.mk
+++ plat/renesas/rz/board/v2h_evk_alpha/rz_board.mk
@@ -7,6 +7,7 @@
 DDR_SOURCES +=	plat/renesas/rz/soc/${PLAT}/drivers/ddr/ddr_setup_lpddr4.c	\
 				plat/renesas/rz/soc/${PLAT}/drivers/ddr/ddr_param_def_lpddr4.c
 
+LPDDR4		:= 1
 DDR_PLL4	:=1600
 
 $(eval $(call add_define,DDR_PLL4))
