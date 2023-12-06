c55e4c8070d0d03aae8e141a2545ea7df94d2b21
Kirk Tiberius Garvey <kirk.garvey.xa@bp.renesas.com>
2023-12-06 09:06:36 +0000

    SSGSP2RZBL-110: T2H: added FIP_ALIGN := 16 to avoid FIP loading errors in eMMC mode

--- plat/renesas/rz/soc/t2h/platform.mk
+++ plat/renesas/rz/soc/t2h/platform.mk
@@ -6,6 +6,7 @@
 
 #Set the platform and SOC specific header files 1st
 PLAT_INCLUDES	:=	-Iplat/renesas/rz/soc/${PLAT}/include
+FIP_ALIGN		:=	16
 
 include plat/renesas/rz/common/t2h_common.mk
 include plat/renesas/rz/board/${PLAT}_${BOARD}/rz_board.mk
