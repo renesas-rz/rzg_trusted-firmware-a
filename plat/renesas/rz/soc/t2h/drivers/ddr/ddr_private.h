8f0df126771df43635f53df762d6fdcdb4911085
Kirk Garvey <kirk.garvey.xa@bp.renesas.com>
2024-02-27 18:57:14 +0900

    2.7.0/rz t2h dev feat ddr streaming debug issues ver2

--- plat/renesas/rz/soc/t2h/drivers/ddr/ddr_private.h
+++ plat/renesas/rz/soc/t2h/drivers/ddr/ddr_private.h
@@ -34,8 +34,6 @@ extern void ddrtop_mc_param_wr(uint32_t addr, uint32_t offset, uint32_t width, u
 extern uint32_t ddrtop_mc_param_rd(uint32_t addr, uint32_t offset, uint32_t width);
 extern void ddrtop_mc_param_poll(uint32_t addr, uint32_t offset, uint32_t width, uint32_t data);
 
-extern void ddrtop_proc_error(void);
-
 extern uint32_t get_mail(uint8_t mode_32bits);
 extern void dwc_ddrphy_phyinit_userCustom_G_waitDone(uint8_t sel_train);
 
