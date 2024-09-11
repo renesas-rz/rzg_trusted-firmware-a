3ee8948376f11a2dac4edfb6a2ed86f5c359d2f8
Nishika Chettry <nishika.chettry.pv@renesas.com>
2024-09-11 18:08:43 +0900

    2.10.0 SSGSP2RZBL-384 - V2H: Variant specific HW IP (GPU, ISP) incorrectly enabled

--- plat/renesas/rz/soc/v2h/include/cpg_regs.h
+++ plat/renesas/rz/soc/v2h/include/cpg_regs.h
@@ -40,11 +40,6 @@
 #define	CPG_PLLDDR1_STBY			(CPG_BASE + 0x100)	/* PLLDDR1standby control register*/
 #define	CPG_PLLDDR1_MON				(CPG_BASE + 0x110)	/* PLLDDR1monitor register */
 
-#define	CPG_PLLGPU_STBY				(CPG_BASE + 0x120)	/* PLLGPU standby control register */
-#define	CPG_PLLGPU_CLK1				(CPG_BASE + 0x124)	/* PLLGPU output clock setting register 1 */
-#define	CPG_PLLGPU_CLK2				(CPG_BASE + 0x128)	/* PLLGPU output clock setting register 2 */
-#define	CPG_PLLGPU_MON				(CPG_BASE + 0x130)	/* PLLGPU monitor register */
-
 #define	CPG_PLLDRP_STBY				(CPG_BASE + 0x140)	/* PLLDRP standby control register */
 #define	CPG_PLLDRP_CLK1				(CPG_BASE + 0x144)	/* PLLDRP output clock setting register 1 */
 #define	CPG_PLLDRP_CLK2				(CPG_BASE + 0x148)	/* PLLDRP output clock setting register 2 */
