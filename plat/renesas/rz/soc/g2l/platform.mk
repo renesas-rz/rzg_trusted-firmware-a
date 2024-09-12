85fd4c4540fc9531f91cec13dd3468ce127eaf53
MichaelAllportWM <michael.allport.wm@renesas.com>
2024-09-12 14:46:00 +0100

    Boot parameter tool addition in build sequence.

--- plat/renesas/rz/soc/g2l/platform.mk
+++ plat/renesas/rz/soc/g2l/platform.mk
@@ -8,6 +8,7 @@ include plat/renesas/rz/common/rz_common.mk
 include plat/renesas/rz/board/${BOARD}/rz_board.mk
 
 PLAT_INCLUDES			+=	-Iplat/renesas/rz/soc/g2l/include
+FIP_ALIGN				:=	16
 
 DDR_SOURCES				+=	plat/renesas/rz/soc/g2l/drivers/ddr/ddr_g2l.c
 
