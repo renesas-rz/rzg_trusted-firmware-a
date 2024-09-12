9981a3b414452398ce5aab3b956fd18b55264a37
MichaelAllportWM <michael.allport.wm@renesas.com>
2024-09-12 14:46:00 +0100

    Boot parameter tool addition in build sequence.

--- plat/renesas/rz/soc/g2l/platform.mk
+++ plat/renesas/rz/soc/g2l/platform.mk
@@ -12,3 +12,17 @@ PLAT_INCLUDES			+=	-Iplat/renesas/rz/soc/g2l/include
 DDR_SOURCES				+=	plat/renesas/rz/soc/g2l/drivers/ddr/ddr_g2l.c
 
 PLAT_BL_COMMON_SOURCES	+=	${DDR_SOURCES}
+
+.PHONY: bptool_make bptool_clean
+
+bptool: bptool_make
+distclean realclean clean: bptool_clean
+
+BPTOOLPATH		?=	tools/renesas/rz_boot_param
+
+bptool_make:
+	${Q}${MAKE} TRUSTED_BOARD_BOOT=${TRUSTED_BOARD_BOOT} --no-print-directory -C ${BPTOOLPATH}
+
+bptool_clean:
+	${Q}${MAKE} --no-print-directory -C ${BPTOOLPATH} clean
+	
