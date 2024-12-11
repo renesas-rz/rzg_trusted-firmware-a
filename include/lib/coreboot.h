jolivain/trusted-firmware-a

feat(optee): add device tree for coreboot table
This adds creation of a device tree that will be passed to OP-TEE.
Currently that device tree only contains the coreboot table per the
Linux coreboot device tree specification. This device tree is then
passed to OP-TEE so it can extract the CBMEM console information from
the coreboot table for logging purposes.

Signed-off-by: Jeffrey Kardatzke <jkardatzke@google.com>
Change-Id: I6a26d335e16f7226018c56ad571cca77b81b0f6a

include/lib/coreboot.h
@@ -41,5 +41,6 @@ typedef enum {
 
 coreboot_memory_t coreboot_get_memory_type(uintptr_t start, size_t size);
 void coreboot_table_setup(void *base);
+void get_coreboot_table_location(uint64_t* address, uint32_t* size);
 
 #endif /* COREBOOT_H */

renesas-rz/rzg_trusted-firmware-a

include/lib/coreboot.h
@@ -41,5 +41,6 @@ typedef enum {
 
 coreboot_memory_t coreboot_get_memory_type(uintptr_t start, size_t size);
 void coreboot_table_setup(void *base);
+void coreboot_get_table_location(uint64_t *address, uint32_t *size);
 
 #endif /* COREBOOT_H */