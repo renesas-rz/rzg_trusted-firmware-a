6c653933a6a58c4a42d1faaf100ecf914dfd6b88
Michael Allport <michael.allport.wm@renesas.com>
2025-05-01 19:58:12 +0900

    feat: G3L add hotplugging

--- plat/renesas/rz/soc/g3l/plat_pm.c
+++ plat/renesas/rz/soc/g3l/plat_pm.c
@@ -18,16 +18,40 @@
 #include <rz_private.h>
 #include <rz_soc_def.h>
 #include <common/bl_common.h>
+#include <cpg_regs.h>
+
+#define LO_REG							(0U)
+#define HI_REG							(1U)
 
 #define SYSTEM_PWR_STATE(s)		((s)->pwr_domain_state[PLAT_MAX_PWR_LVL])
 #define CLUSTER_PWR_STATE(s)	((s)->pwr_domain_state[MPIDR_AFFLVL1])
 #define CORE_PWR_STATE(s)		((s)->pwr_domain_state[MPIDR_AFFLVL0])
 
-static void rz_program_trusted_mailbox(uintptr_t address)
-{
-	uintptr_t *mailbox = (uintptr_t *) PLAT_TRUSTED_MAILBOX_BASE;
+#define SYS_LP_CTL1_CA55SLEEP_REQ				0x100
 
-	*mailbox = address;
+typedef struct {
+	uintptr_t reg_pchctl;
+	uintptr_t reg_pchmon;
+} CPG_CORE_PWR;
+
+typedef unsigned long aligned_mailbox_value_t __aligned(CACHE_WRITEBACK_GRANULE);
+
+typedef struct {
+	aligned_mailbox_value_t value; /* Mailbox must be cache aligned */
+} mailbox_t;
+
+uintptr_t	gp_warm_ep;
+
+static void rz_program_trusted_mailbox(u_register_t mpidr, uintptr_t address)
+{
+	mailbox_t *mailbox = (mailbox_t *) PLAT_TRUSTED_MAILBOX_BASE;
+	uint64_t linear_id = plat_core_pos_by_mpidr(mpidr);
+	unsigned long range;
+
+	mailbox[linear_id].value = address;
+	range = (unsigned long)&mailbox[linear_id];
+
+	flush_dcache_range(range, sizeof(range));
 }
 
 static void rz_cpu_standby(plat_local_state_t cpu_state)
@@ -128,11 +152,101 @@ static void __dead2 rz_system_off(void)
 	panic();
 }
 
+static int rzg3l_pwr_domain_on(u_register_t mpidr)
+{
+	const uint32_t rval[PLATFORM_CORE_COUNT][2] = {
+		{ SYS_CA55_CFG_RVAL0, SYS_CA55_CFG_RVAH0 },
+		{ SYS_CA55_CFG_RVAL1, SYS_CA55_CFG_RVAH1 },
+		{ SYS_CA55_CFG_RVAL2, SYS_CA55_CFG_RVAH2 },
+		{ SYS_CA55_CFG_RVAL3, SYS_CA55_CFG_RVAH3 }
+	};
+
+	const CPG_CORE_PWR pch[PLATFORM_CORE_COUNT] = {
+		{ CPG_CORE0_PCHCTL, CPG_CORE0_PCHMON },
+		{ CPG_CORE1_PCHCTL, CPG_CORE1_PCHMON },
+		{ CPG_CORE2_PCHCTL, CPG_CORE2_PCHMON },
+		{ CPG_CORE3_PCHCTL, CPG_CORE3_PCHMON }
+	};
+
+	uint8_t coreid = MPIDR_AFFLVL1_VAL(mpidr);
+
+	if (coreid >= PLATFORM_CORE_COUNT)
+		return PSCI_E_INVALID_PARAMS;
+
+	/* Check if in standby */
+	if ((mmio_read_32(SYS_LP_CTL2) & 0x1) == 0x1) {
+		mmio_write_32(pch[coreid].reg_pchctl, CORE0_PCHCTL_PREQ0_SET);
+		while (((mmio_read_32(pch[coreid].reg_pchmon) & CORE0_PCHMON_PACCEPT0_MON) != CORE0_PCHMON_PACCEPT0_MON))
+			;
+		mmio_write_32(pch[coreid].reg_pchctl, 0x00000000);
+		while ((mmio_read_32(pch[coreid].reg_pchmon) & CORE0_PCHMON_PACCEPT0_MON) != 0x0)
+			;
+	}
+
+	rz_program_trusted_mailbox(mpidr, gp_warm_ep);
+
+	/*  Start the core */
+	mmio_write_32(rval[coreid][LO_REG], (uint32_t)(gp_warm_ep & 0xFFFFFFFC));
+	mmio_write_32(rval[coreid][HI_REG], (uint32_t)((gp_warm_ep >> 32) & 0xFF));
+
+	/* Assert PORESET */
+	mmio_write_32(CPG_RST_CA55, (0x00010000 << coreid));
+	while ((mmio_read_32(CPG_RSTMON_CA55) & (0x1 << coreid)) == 0x0)
+		;
+
+	/* Deassert PORESET and RERESET */
+	mmio_write_32(CPG_RST_CA55, (0x00110011 << coreid));
+	while ((mmio_read_32(CPG_RSTMON_CA55) & (0x1 << coreid)) != 0x0)
+		;
+
+	mmio_write_32(pch[coreid].reg_pchctl, (CORE0_PCHCTL_PREQ0_SET | CORE0_PCHCTL_PSTATE0_SET_ON));
+	while (((mmio_read_32(pch[coreid].reg_pchmon) & CORE0_PCHMON_PACCEPT0_MON) != CORE0_PCHMON_PACCEPT0_MON))
+		;
+
+	mmio_write_32(pch[coreid].reg_pchctl, CORE0_PCHCTL_PSTATE0_SET_ON);
+	while ((mmio_read_32(pch[coreid].reg_pchmon) & CORE0_PCHMON_PACCEPT0_MON) != 0x0)
+		;
+
+	return PSCI_E_SUCCESS;
+}
+
+static void rzg3l_pwr_domain_on_finish(const psci_power_state_t *target_state)
+{
+	plat_gic_pcpu_init();
+	plat_gic_cpuif_enable();
+}
+
+static void rzg3l_pwr_domain_off(const psci_power_state_t *state)
+{
+	unsigned long mpidr = read_mpidr_el1();
+	uint8_t coreid = MPIDR_AFFLVL1_VAL(mpidr);
+
+	if (coreid >= PLATFORM_CORE_COUNT)
+		return;
+
+	/* Prevent interrupts from spuriously waking up this cpu */
+	plat_gic_cpuif_disable();
+
+	/* Request transition to Cortex-A55 CoreX Sleep Mode */
+	mmio_write_32(SYS_LP_CTL1, (SYS_LP_CTL1_CA55SLEEP_REQ << coreid));
+
+
+	/* Enter the Cortex-A55 Sleep Mode */
+	mmio_write_32(SYS_LP_CTL2, mmio_read_32(SYS_LP_CTL2) | 0x00000001);
+
+	/* Issue Barrier instruction */
+	isb();
+	dsb();
+	dcsw_op_all(DCCISW);
+
+	/* A WFI instruction will be executed via lib/psci/psci_off.c->psci_power_down_wfi() */
+}
+
 const plat_psci_ops_t rz_plat_psci_ops = {
 	.cpu_standby						= rz_cpu_standby,
-	.pwr_domain_on						= NULL,
-	.pwr_domain_on_finish				= NULL,
-	.pwr_domain_off						= NULL,
+	.pwr_domain_on						= rzg3l_pwr_domain_on,
+	.pwr_domain_on_finish				= rzg3l_pwr_domain_on_finish,
+	.pwr_domain_off						= rzg3l_pwr_domain_off,
 	.pwr_domain_suspend					= rz_pwr_domain_suspend,
 	.pwr_domain_suspend_finish			= rz_pwr_domain_suspend_finish,
 	.pwr_domain_pwr_down_wfi			= rz_pwr_domain_pwr_down_wfi,
@@ -146,7 +260,7 @@ const plat_psci_ops_t rz_plat_psci_ops = {
 
 int plat_setup_psci_ops(uintptr_t sec_entrypoint, const plat_psci_ops_t **psci_ops)
 {
-	rz_program_trusted_mailbox(sec_entrypoint);
+	gp_warm_ep = sec_entrypoint;
 	*psci_ops = &rz_plat_psci_ops;
 	return 0;
 }
