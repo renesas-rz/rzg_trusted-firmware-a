415a7dc9ee9ed509d0994ed456de6a39bf1d2a35
Nishika <nishika.chettry.pv@renesas.com>
2025-06-16 15:17:49 +0100

    set a default setting of S_PRIV

--- plat/renesas/rz/soc/t2h/plat_security.c
+++ plat/renesas/rz/soc/t2h/plat_security.c
@@ -117,7 +117,7 @@ static void plat_tzc_ddr_setup(void)
 			.base = 0,	/* Not Used by Region 0 */
 			.end  = 0,	/* Not Used by Region 0 */
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_UNPRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
 		},
 
 		{}
@@ -129,7 +129,7 @@ static void plat_tzc_ddr_setup(void)
 			.base = 0,	/* Not Used by Region 0 */
 			.end  = 0,	/* Not Used by Region 0 */
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_UNPRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
 		},
 
 		{}
@@ -159,7 +159,7 @@ static void plat_tzc_xspi_setup(void)
 			.base = RZT2H_XSPI0_MEMORY_MAP_BASE,
 			.end  = RZT2H_XSPI0_MEMORY_MAP_BASE + RZT2H_XSPI_MEMORY_MAP_SIZE - 1ULL,
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_UNPRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
 		},
 
 		{
@@ -167,7 +167,7 @@ static void plat_tzc_xspi_setup(void)
 			.base = RZT2H_XSPI1_MEMORY_MAP_BASE,
 			.end  = RZT2H_XSPI1_MEMORY_MAP_BASE + RZT2H_XSPI_MEMORY_MAP_SIZE - 1ULL,
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_UNPRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
 		},
 
 		{}
@@ -218,7 +218,7 @@ static void plat_tzc_bsc_setup(void)
 			.base = 0,	/* Not Used by Region 0 */
 			.end  = 0,	/* Not Used by Region 0 */
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_PRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
 		},
 
 		{}
