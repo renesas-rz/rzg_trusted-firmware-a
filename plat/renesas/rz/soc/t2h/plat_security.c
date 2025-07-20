53d3f76a8cf35a67c8c3558bc51269c0d82b3b32
Nishika <nishika.chettry.pv@renesas.com>
2025-07-20 23:03:34 +0100

    changing DDR R bus to be NS_UNPRIV

--- plat/renesas/rz/soc/t2h/plat_security.c
+++ plat/renesas/rz/soc/t2h/plat_security.c
@@ -129,7 +129,7 @@ static void plat_tzc_ddr_setup(void)
 			.base = 0,	/* Not Used by Region 0 */
 			.end  = 0,	/* Not Used by Region 0 */
 			.sec_attr = PLAT_TZC_SEC_ATTR_REG_UNUSED_VAL,
-			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_S_PRIV
+			.nsaid_permissions = PLAT_TZC_REGION_ACCESS_NS_UNPRIV
 		},
 
 		{}
