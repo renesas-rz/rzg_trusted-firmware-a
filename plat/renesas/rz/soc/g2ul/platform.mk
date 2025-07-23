8765908a77d0a0505fd34641064c7f316387d4e8
Michael Allport <michael.allport.wm@renesas.com>
2025-07-23 20:45:11 +0900

    Adding FIP align for the g2l, g2ul and v2l

--- plat/renesas/rz/soc/g2ul/platform.mk
+++ plat/renesas/rz/soc/g2ul/platform.mk
@@ -8,6 +8,7 @@ include plat/renesas/rz/common/rz_common.mk
 include plat/renesas/rz/board/${BOARD}/rz_board.mk
 
 PLAT_INCLUDES			+=	-Iplat/renesas/rz/soc/g2ul/include
+FIP_ALIGN				:=	16
 
 DDR_SOURCES				+=	plat/renesas/rz/soc/g2ul/drivers/ddr/ddr_g2ul.c
 
@@ -34,3 +35,16 @@ bptool_make:
 
 bptool_clean:
 	${Q}${MAKE} --no-print-directory -C ${BPTOOLPATH} clean
+
+pkg:
+	./tools/renesas/bptool build/g2ul/${BUILD_TYPE}/bl2.bin build/g2ul/${BUILD_TYPE}/bp_spi.bin 0x00012000 spi
+	cat build/g2ul/${BUILD_TYPE}/bp_spi.bin build/g2ul/${BUILD_TYPE}/bl2.bin > build/g2ul/${BUILD_TYPE}/bl2_bp_spi.bin
+	objcopy -I binary -O srec --adjust-vma=0x11E00 --srec-forceS3 build/g2ul/${BUILD_TYPE}/bl2_bp_spi.bin  build/g2ul/${BUILD_TYPE}/bl2_bp_spi.srec
+	./tools/renesas/bptool build/g2ul/${BUILD_TYPE}/bl2.bin build/g2ul/${BUILD_TYPE}/bp_mmc.bin 0x00012000 mmc
+	cat build/g2ul/${BUILD_TYPE}/bp_mmc.bin build/g2ul/${BUILD_TYPE}/bl2.bin > build/g2ul/${BUILD_TYPE}/bl2_bp_mmc.bin
+	objcopy -I binary -O srec --adjust-vma=0x11E00 --srec-forceS3 build/g2ul/${BUILD_TYPE}/bl2_bp_mmc.bin  build/g2ul/${BUILD_TYPE}/bl2_bp_mmc.srec
+	./tools/renesas/bptool build/g2ul/${BUILD_TYPE}/bl2.bin build/g2ul/${BUILD_TYPE}/bp_esd.bin 0x00012000 esd
+	cat build/g2ul/${BUILD_TYPE}/bp_esd.bin build/g2ul/${BUILD_TYPE}/bl2.bin > build/g2ul/${BUILD_TYPE}/bl2_bp_esd.bin
+	objcopy -I binary -O srec --adjust-vma=0x11E00 --srec-forceS3 build/g2ul/${BUILD_TYPE}/bl2_bp_esd.bin  build/g2ul/${BUILD_TYPE}/bl2_bp_esd.srec
+	#Generate FIP S-Record if FIP binary is present
+	if [ -f build/g2ul/${BUILD_TYPE}/fip.bin ]; then  objcopy -I binary -O srec --adjust-vma=0x00000 --srec-forceS3 build/g2ul/${BUILD_TYPE}/fip.bin build/g2ul/${BUILD_TYPE}/fip.srec ; fi ;
