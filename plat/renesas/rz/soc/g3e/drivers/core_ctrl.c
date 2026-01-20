5f06a851511542e7db59564183d3f9e18d8adbc6
Michael Allport <michael.allport.wm@renesas.com>
2026-01-20 21:52:14 +0900

    G3E hotplugging fix

---
+++ plat/renesas/rz/soc/g3e/drivers/core_ctrl.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2024, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <arch_helpers.h>
+#include <core_ctrl.h>
+#include <cpg_regs.h>
+#include <lib/mmio.h>
+#include <rz_soc_def.h>
+#include <platform_def.h>
+#include <common/debug.h>
+#include <lib/utils_def.h>
+
+/* External Reset Application */
+#define NUM_STEPS_APPLY_EXT_WARM_RST		(7)
+#define NUM_STEPS_RELEASE_EXT_WARM_RST		(6)
+#define NUM_STEPS_CORE_STARUP				(4)
+
+typedef enum {
+	RST_ACTION_WRITE	= 0,
+	RST_ACTION_READ
+} rst_action_t;
+
+typedef struct {
+	uintptr_t reg;
+	uint32_t  step_mask;
+	uint32_t  step_value;
+	rst_action_t action;
+} reset_steps_t;
+
+/* Warm external reset apply steps matrix */
+static const reset_steps_t warm_ext_reset_apply[PLATFORM_CORE_COUNT][NUM_STEPS_APPLY_EXT_WARM_RST] = {
+	/* Core 0 */
+	{
+		{ CPG_LP_CA55_CTL6, 0x1,		0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x7F,		0x1,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2, 0x1FF,		0x101,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x7F,		0x0,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2, 0x1FF,		0x0,		RST_ACTION_READ},
+		{ CPG_RST_0,		0x100010,	0x100000,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x10,		0x10,		RST_ACTION_READ}
+	},
+	/* Core 1 */
+	{
+		{ CPG_LP_CA55_CTL6, 0x2,		0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x7F0000,	0x10000,	RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2, 0x1FF0000,	0x1010000,	RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x7F0000,	0x0,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2, 0x1FF0000,	0x0,		RST_ACTION_READ},
+		{ CPG_RST_0,		0x200020,	0x200000,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x20,		0x20,		RST_ACTION_READ}
+	},
+#if PLATFORM_CORE_COUNT > 2
+	/* Core 2 */
+	{
+		{ CPG_LP_CA55_CTL6, 0x4,		0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F,		0x1,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF,		0x101,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F,		0x0,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF,		0x0,		RST_ACTION_READ},
+		{ CPG_RST_0,		0x400040,	0x400000,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x40,		0x40,		RST_ACTION_READ}
+	},
+
+	/* Core 3 */
+	{
+		{ CPG_LP_CA55_CTL6, 0x8,		0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F0000,	0x10000,	RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF0000,	0x1010000,	RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F0000,	0x0,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF0000,	0x0,		RST_ACTION_READ},
+		{ CPG_RST_0,		0x800080,	0x800000,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x80,		0x80,		RST_ACTION_READ}
+	}
+#endif
+};
+
+/* Warm external reset release steps matrix */
+static  const reset_steps_t warm_ext_reset_release[PLATFORM_CORE_COUNT][NUM_STEPS_RELEASE_EXT_WARM_RST] = {
+	/* Core 0 */
+	{
+		{ CPG_LP_CA55_CTL2,	0x7F,		0x11,		RST_ACTION_WRITE},
+		{ CPG_RST_0,		0x3FFF3FFF,	0x3FFF3FFF,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x1FFFF,	0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2,	0x1FF,		0x111,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2,	0x7F,		0x10,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2,	0x1FF,		0x10,		RST_ACTION_READ},
+
+	},
+	/* Core 1 */
+	{
+		{ CPG_LP_CA55_CTL2, 0x7F0000,	0x110000,	RST_ACTION_WRITE},
+		{ CPG_RST_0,		0x3FFF3FFF,	0x3FFF3FFF,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x1FFFF,	0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x1FF0000,	0x1110000,	RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL2, 0x7F0000,	0x100000,	RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL2, 0x1FF0000,	0x100000,	RST_ACTION_READ},
+	},
+#if PLATFORM_CORE_COUNT > 2
+	/* Core 2 */
+	{
+		{ CPG_LP_CA55_CTL3, 0x7F,		0x11,		RST_ACTION_WRITE},
+		{ CPG_RST_0,		0x3FFF3FFF,	0x3FFF3FFF, RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x1FFFF,	0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x1FF,		0x111,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F,		0x10,		RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF,		0x10,		RST_ACTION_READ},
+	},
+
+	/* Core 3 */
+	{
+		{ CPG_LP_CA55_CTL3, 0x7F0000,	0x110000,	RST_ACTION_WRITE},
+		{ CPG_RST_0,		0x3FFF3FFF,	0x3FFF3FFF,	RST_ACTION_WRITE},
+		{ CPG_RSTMON_0,		0x1FFFF,	0x0,		RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x1FF0000,	0x1110000,	RST_ACTION_READ},
+		{ CPG_LP_CA55_CTL3, 0x7F0000,	0x100000,	RST_ACTION_WRITE},
+		{ CPG_LP_CA55_CTL3, 0x1FF0000,	0x100000,	RST_ACTION_READ},
+	}
+#endif
+};
+
+static void perform_control_sequence(const reset_steps_t reset_seq[], uint8_t num_steps)
+{
+	uint8_t i;
+
+	for (i = 0; i < num_steps; i++) {
+		reset_steps_t step = reset_seq[i];
+
+		if (step.action == RST_ACTION_WRITE) {
+			uint32_t reg_val = mmio_read_32(step.reg);
+
+			reg_val &= (~step.step_mask);
+			reg_val |= step.step_value;
+			mmio_write_32(step.reg, reg_val);
+		} else {
+			while ((mmio_read_32(step.reg) & step.step_mask) != step.step_value)
+				;
+		}
+	}
+}
+
+void core_ctrl_set_in_standby(uint32_t coreid)
+{
+	/* Request transition to Cortex-A55 CoreX Sleep Mode */
+	mmio_write_32(CPG_LP_CTL1, mmio_read_32(CPG_LP_CTL1) | (CPG_LP_CTL1_CA55SLEEP_REQ << coreid));
+
+	/* Issue Barrier instruction */
+	isb();
+	dsb();
+	dcsw_op_all(DCCISW);
+}
+
+void core_ctrl_warm_boot(uint32_t coreid)
+{
+	/* If in standby, then clear standby mode. */
+	if ((mmio_read_32(CPG_LP_CTL1) & (CPG_LP_CTL1_CA55SLEEP_ACK << coreid)) != 0) {
+		/* Clear CA55SLEEP_REQ and CA55SLEEP_ACK */
+		mmio_write_32(CPG_LP_CTL1, mmio_read_32(CPG_LP_CTL1) & ~((CPG_LP_CTL1_CA55SLEEP_ACK | CPG_LP_CTL1_CA55SLEEP_REQ) << coreid));
+	}
+
+	/*
+	 * Warm external reset apply.
+	 * Necessary to apply RVBARADDR0/1/2/3 change.
+	 */
+	perform_control_sequence(&warm_ext_reset_apply[coreid][0], NUM_STEPS_APPLY_EXT_WARM_RST);
+
+	/* Warm external reset release */
+	perform_control_sequence(&warm_ext_reset_release[coreid][0], NUM_STEPS_RELEASE_EXT_WARM_RST);
+}
--- plat/renesas/rz/soc/g3e/plat_pm.c
+++ plat/renesas/rz/soc/g3e/plat_pm.c
@@ -13,8 +13,8 @@
 #include <plat/common/platform.h>
 #include <common/bl_common.h>
 
-#include "cpg_regs.h"
 #include <cpg.h>
+#include <core_ctrl.h>
 #include "pwrc.h"
 #include "rz_private.h"
 #include "rz_soc_def.h"
@@ -47,11 +47,11 @@ typedef struct {
 uintptr_t	gp_warm_ep;
 
 const uint32_t cores_reset_vector[PLATFORM_CORE_COUNT][2] = {
-		{ SYS_ACPU_CFG_RVAL0, SYS_ACPU_CFG_RVAH0 },
-		{ SYS_ACPU_CFG_RVAL1, SYS_ACPU_CFG_RVAH1 },
-		{ SYS_ACPU_CFG_RVAL2, SYS_ACPU_CFG_RVAH2 },
-		{ SYS_ACPU_CFG_RVAL3, SYS_ACPU_CFG_RVAH3 }
-	};
+	{ SYS_ACPU_CFG_RVAL0, SYS_ACPU_CFG_RVAH0 },
+	{ SYS_ACPU_CFG_RVAL1, SYS_ACPU_CFG_RVAH1 },
+	{ SYS_ACPU_CFG_RVAL2, SYS_ACPU_CFG_RVAH2 },
+	{ SYS_ACPU_CFG_RVAL3, SYS_ACPU_CFG_RVAH3 }
+};
 
 static void rz_program_trusted_mailbox(u_register_t mpidr, uintptr_t address)
 {
@@ -97,55 +97,20 @@ static int rz_validate_power_state(unsigned int power_state, psci_power_state_t 
 
 static int rzg3e_pwr_domain_on(u_register_t mpidr)
 {
-	const CPG_CORE_PWR pch[PLATFORM_CORE_COUNT] = {
-		{ CPG_LP_CA55_CTL2, CPG_LP_CA55_CTL2_COREPREQ0, CPG_LP_CA55_CTL2_COREACCEPT0, CPG_LP_CA55_CTL2_CORESTATE0_ON_MASK },
-		{ CPG_LP_CA55_CTL2, CPG_LP_CA55_CTL2_COREPREQ1, CPG_LP_CA55_CTL2_COREACCEPT1, CPG_LP_CA55_CTL2_CORESTATE1_ON_MASK },
-		{ CPG_LP_CA55_CTL3, CPG_LP_CA55_CTL3_COREPREQ2, CPG_LP_CA55_CTL3_COREACCEPT2, CPG_LP_CA55_CTL3_CORESTATE2_ON_MASK },
-		{ CPG_LP_CA55_CTL3, CPG_LP_CA55_CTL3_COREPREQ3, CPG_LP_CA55_CTL3_COREACCEPT3, CPG_LP_CA55_CTL3_CORESTATE3_ON_MASK }
-	};
 
 	uint8_t coreid = MPIDR_AFFLVL1_VAL(mpidr);
 
 	if (coreid >= PLATFORM_CORE_COUNT)
 		return PSCI_E_INVALID_PARAMS;
 
-	/* Check if in standby then reset */
-	if ((mmio_read_32(CPG_LP_CTL1) & (CPG_LP_CTL1_CA55SLEEP_ACK << coreid))) {
-		/* Clear CA55SLEEP_REQ and CA55SLEEP_ACK */
-		mmio_write_32(CPG_LP_CTL1, mmio_read_32(CPG_LP_CTL1) & ~((CPG_LP_CTL1_CA55SLEEP_ACK | CPG_LP_CTL1_CA55SLEEP_REQ) << coreid));
-
-		/* Reset */
-		mmio_write_32(pch[coreid].reg, pch[coreid].preq_mask);
-		while ((mmio_read_32(pch[coreid].reg) & pch[coreid].paccept_mask) != pch[coreid].paccept_mask)
-			;
-		mmio_write_32(pch[coreid].reg, 0x00000000);
-		while ((mmio_read_32(pch[coreid].reg) & pch[coreid].paccept_mask) != 0x0)
-			;
-	}
-
 	rz_program_trusted_mailbox(mpidr, gp_warm_ep);
 
-	/*  Start the core */
+	/*  Set Reset Vector */
 	mmio_write_32(cores_reset_vector[coreid][LO_REG], (uint32_t)(gp_warm_ep & 0xFFFFFFFC));
 	mmio_write_32(cores_reset_vector[coreid][HI_REG], (uint32_t)((gp_warm_ep >> 32) & 0xFF));
 
-	/* Assert PORESET */
-	mmio_write_32(CPG_RST_0, (0x00010000 << coreid));
-	while ((mmio_read_32(CPG_RSTMON_0) & (0x1 << coreid)) == 0x0)
-		;
-
-	/* Deassert PORESET and RERESET */
-	mmio_write_32(CPG_RST_0, (0x00110011 << coreid));
-	while ((mmio_read_32(CPG_RSTMON_0) & (0x1 << coreid)) != 0x0)
-		;
-
-	mmio_write_32(pch[coreid].reg, (pch[coreid].pstate_on_mask | pch[coreid].preq_mask));
-	while ((mmio_read_32(pch[coreid].reg) & pch[coreid].paccept_mask) != pch[coreid].paccept_mask)
-		;
-
-	mmio_write_32(pch[coreid].reg, pch[coreid].pstate_on_mask);
-	while ((mmio_read_32(pch[coreid].reg) & pch[coreid].paccept_mask) != 0x0)
-		;
+	/* Reset applied to set */
+	core_ctrl_warm_boot(coreid);
 
 	return PSCI_E_SUCCESS;
 }
@@ -167,13 +132,7 @@ static void rzg3e_pwr_domain_off(const psci_power_state_t *state)
 	/* Prevent interrupts from spuriously waking up this cpu */
 	plat_gic_cpuif_disable();
 
-	/* Request transition to Cortex-A55 CoreX Sleep Mode */
-	mmio_write_32(CPG_LP_CTL1, (CPG_LP_CTL1_CA55SLEEP_REQ << coreid));
-
-	/* Issue Barrier instruction */
-	isb();
-	dsb();
-	dcsw_op_all(DCCISW);
+	core_ctrl_set_in_standby(coreid);
 
 	/* A WFI instruction will be executed via lib/psci/psci_off.c->psci_power_down_wfi() */
 }
